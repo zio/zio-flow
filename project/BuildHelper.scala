df7b7949e9ba338f67161ca1a161d9733c87d6fd
Dejan Mijic <dmijic@acm.org>
2021-02-04 20:18:20 +0100

    Update build helper

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -11,10 +11,10 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
   val Scala211        = "2.11.12"
-  val Scala212        = "2.12.12"
-  val Scala213        = "2.13.3"
-  val ScalaDotty      = "0.27.0-RC1"
-  val SilencerVersion = "1.7.1"
+  val Scala212        = "2.12.13"
+  val Scala213        = "2.13.4"
+  val ScalaDotty      = "3.0.0-M3"
+  val SilencerVersion = "1.7.2"
 
   private val stdOptions = Seq(
     "-deprecation",
@@ -127,11 +127,6 @@ object BuildHelper {
 
   def extraOptions(scalaVersion: String, optimize: Boolean) =
     CrossVersion.partialVersion(scalaVersion) match {
-      case Some((0, _))  =>
-        Seq(
-          "-language:implicitConversions",
-          "-Xignore-scala2-macros"
-        )
       case Some((2, 13)) =>
         Seq(
           "-Ywarn-unused:params,-implicits"
@@ -169,6 +164,11 @@ object BuildHelper {
           "-Xmax-classfile-name",
           "242"
         ) ++ std2xOptions
+      case _ if isDotty  =>
+        Seq(
+          "-language:implicitConversions",
+          "-Xignore-scala2-macros"
+        )
       case _             => Seq.empty
     }
 
@@ -223,7 +223,7 @@ object BuildHelper {
         Seq(
           "com.github.ghik" % "silencer-lib" % SilencerVersion % Provided cross CrossVersion.full,
           compilerPlugin("com.github.ghik" % "silencer-plugin" % SilencerVersion cross CrossVersion.full),
-          compilerPlugin("org.typelevel"  %% "kind-projector"  % "0.11.1" cross CrossVersion.full)
+          compilerPlugin("org.typelevel"  %% "kind-projector"  % "0.11.3" cross CrossVersion.full)
         )
     },
     semanticdbEnabled := !isDotty.value, // enable SemanticDB
