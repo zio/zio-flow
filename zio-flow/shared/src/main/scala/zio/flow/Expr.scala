2e659833f6f04353ce5e03a7135cdc2933804458
ashprakasan <prakasanaiswarya@gmail.com>
2021-02-10 02:00:34 +0530

    Make if-else lazy.

--- zio-flow/shared/src/main/scala/zio/flow/Expr.scala
+++ zio-flow/shared/src/main/scala/zio/flow/Expr.scala
@@ -59,7 +59,7 @@ sealed trait Expr[+A] { self =>
     Expr.Fold(self.widen[List[A0]], initial, (tuple: Expr[(B, A0)]) => f(tuple._1, tuple._2))
 
   final def ifThenElse[B](ifTrue: Expr[B], ifFalse: Expr[B])(implicit ev: A <:< Boolean): Expr[B] =
-    Expr.Branch(self.widen[Boolean], ifTrue, ifFalse)
+    Expr.Branch(self.widen[Boolean], () => ifTrue, () => ifFalse)
 
   final def iterate[A1 >: A](iterate: Expr[A1] => Expr[A1])(predicate: Expr[A1] => Expr[Boolean]): Expr[A1] =
     Expr.Iterate(self, iterate, predicate)
@@ -81,21 +81,21 @@ sealed trait Expr[+A] { self =>
     Expr.Not(self.widen[Boolean])
 }
 object Expr           {
-  final case class Literal[A](value: A, schema: Schema[A])                                          extends Expr[A]
-  final case class Ignore[A](value: Expr[A])                                                        extends Expr[Unit]
-  final case class Variable[A](identifier: String)                                                  extends Expr[A]
-  final case class AddInt(left: Expr[Int], right: Expr[Int])                                        extends Expr[Int]
-  final case class DivInt(left: Expr[Int], right: Expr[Int])                                        extends Expr[Int]
-  final case class Either0[A, B](either: Either[Expr[A], Expr[B]])                                  extends Expr[Either[A, B]]
-  final case class Tuple2[A, B](left: Expr[A], right: Expr[B])                                      extends Expr[(A, B)]
-  final case class Tuple3[A, B, C](_1: Expr[A], _2: Expr[B], _3: Expr[C])                           extends Expr[(A, B, C)]
-  final case class First[A, B](tuple: Expr[(A, B)])                                                 extends Expr[A]
-  final case class Second[A, B](tuple: Expr[(A, B)])                                                extends Expr[B]
-  final case class Branch[A](predicate: Expr[Boolean], ifTrue: Expr[A], ifFalse: Expr[A])           extends Expr[A]
-  final case class LessThanEqual[A](left: Expr[A], right: Expr[A], sortable: Sortable[A])           extends Expr[Boolean]
-  final case class Not[A](value: Expr[Boolean])                                                     extends Expr[Boolean]
-  final case class And[A](left: Expr[Boolean], right: Expr[Boolean])                                extends Expr[Boolean]
-  final case class Fold[A, B](list: Expr[List[A]], initial: Expr[B], body: Expr[(B, A)] => Expr[B]) extends Expr[B]
+  final case class Literal[A](value: A, schema: Schema[A])                                            extends Expr[A]
+  final case class Ignore[A](value: Expr[A])                                                          extends Expr[Unit]
+  final case class Variable[A](identifier: String)                                                    extends Expr[A]
+  final case class AddInt(left: Expr[Int], right: Expr[Int])                                          extends Expr[Int]
+  final case class DivInt(left: Expr[Int], right: Expr[Int])                                          extends Expr[Int]
+  final case class Either0[A, B](either: Either[Expr[A], Expr[B]])                                    extends Expr[Either[A, B]]
+  final case class Tuple2[A, B](left: Expr[A], right: Expr[B])                                        extends Expr[(A, B)]
+  final case class Tuple3[A, B, C](_1: Expr[A], _2: Expr[B], _3: Expr[C])                             extends Expr[(A, B, C)]
+  final case class First[A, B](tuple: Expr[(A, B)])                                                   extends Expr[A]
+  final case class Second[A, B](tuple: Expr[(A, B)])                                                  extends Expr[B]
+  final case class Branch[A](predicate: Expr[Boolean], ifTrue: () => Expr[A], ifFalse: () => Expr[A]) extends Expr[A]
+  final case class LessThanEqual[A](left: Expr[A], right: Expr[A], sortable: Sortable[A])             extends Expr[Boolean]
+  final case class Not[A](value: Expr[Boolean])                                                       extends Expr[Boolean]
+  final case class And[A](left: Expr[Boolean], right: Expr[Boolean])                                  extends Expr[Boolean]
+  final case class Fold[A, B](list: Expr[List[A]], initial: Expr[B], body: Expr[(B, A)] => Expr[B])   extends Expr[B]
   final case class Iterate[A](initial: Expr[A], iterate: Expr[A] => Expr[A], predicate: Expr[A] => Expr[Boolean])
       extends Expr[A]
 
