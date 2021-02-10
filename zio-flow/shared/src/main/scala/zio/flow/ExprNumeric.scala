0883137769f207455847a60709bc4519c34650dc
ashprakasan <prakasanaiswarya@gmail.com>
2021-02-10 13:14:12 +0530

    Naive addition of operations to Numeric type.

--- zio-flow/shared/src/main/scala/zio/flow/ExprNumeric.scala
+++ zio-flow/shared/src/main/scala/zio/flow/ExprNumeric.scala
@@ -8,4 +8,11 @@ trait ExprNumeric[+A] {
 
   final def /[A1 >: A](that: Expr[A1])(implicit numeric: Numeric[A1]): Expr[A1] =
     Expr.DivNumeric(self.widen[A1], that, numeric)
+
+  final def *[A1 >: A](that: Expr[A1])(implicit numeric: Numeric[A1]): Expr[A1] =
+    Expr.MulNumeric(self.widen[A1], that, numeric)
+
+  final def -[A1 >: A](that: Expr[A1])(implicit numeric: Numeric[A1]): Expr[A1] =
+    Expr.SubNumeric(self.widen[A1], that, numeric)
+
 }
