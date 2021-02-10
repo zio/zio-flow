33dde2a6b8075f5a21de99034bd7a3238677285f
ashprakasan <prakasanaiswarya@gmail.com>
2021-02-10 15:35:39 +0530

    Expand list of supported operations for 'ExprNumeric'

--- zio-flow/shared/src/main/scala/zio/flow/ExprNumeric.scala
+++ zio-flow/shared/src/main/scala/zio/flow/ExprNumeric.scala
@@ -11,6 +11,17 @@ trait ExprNumeric[+A] { self: Expr[A] =>
     Expr.MulNumeric(self.widen[A1], that, numeric)
 
   final def -[A1 >: A](that: Expr[A1])(implicit numeric: Numeric[A1]): Expr[A1] =
-    Expr.SubNumeric(self.widen[A1], that, numeric)
+    Expr.DiffNumeric(self.widen[A1], that, numeric)
 
+  final def pow[A1 >: A](that: Expr[A1])(implicit numeric: Numeric[A1]): Expr[A1] =
+    Expr.PowNumeric(self.widen[A1], that, numeric)
+
+  implicit val intZero: Expr[Int]       = Expr(0)
+  implicit val shortZero: Expr[Short]   = Expr(0)
+  implicit val longZero: Expr[Long]     = Expr(0L)
+  implicit val floatZero: Expr[Float]   = Expr(0.0f)
+  implicit val doubleZero: Expr[Double] = Expr(0.0)
+
+  final def unary_-[A1 >: A](implicit numeric: Numeric[A1], zero: Expr[A1]): Expr[A1] =
+    Expr.DiffNumeric(zero, self.widen[A1], numeric)
 }
