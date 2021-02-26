215484ef84b4abed3df7d30628a280af3b399c72
ashprakasan <prakasanaiswarya@gmail.com>
2021-02-26 19:10:40 +0530

    Add uber-eats example.

--- zio-flow/shared/src/main/scala/zio/flow/Example.scala
+++ zio-flow/shared/src/main/scala/zio/flow/Example.scala
@@ -1,6 +1,7 @@
 package zio.flow
 
 object Example {
+
   import Constructor._
 
   // Expr[A] => Expr[(B, A)]
@@ -33,8 +34,7 @@ object Example {
 }
 
 object EmailCampaign {
-  type City = String
-
+  type City       = String
   type Cuisine    = String
   type Review     = Int
   type Restaurant = String
@@ -57,7 +57,7 @@ object EmailCampaign {
   /**
    * 1. Get reviews of restaurants in a certain category (e.g. "Asian").
    * 2. If there are 2 days of good reviews, we send a coupon to people
-   *    in that city for restaurants in that category.
+   * in that city for restaurants in that category.
    */
   lazy val emailCampaign: ZFlow[(City, Cuisine), Throwable, Any] = {
     def waitForPositiveReviews(restaurants: Expr[List[Restaurant]]) =
@@ -79,4 +79,130 @@ object EmailCampaign {
       _           <- sendEmail(???)
     } yield ()
   }
+}
+
+/**
+ * A real-world example that models the workflow of uber-eats.
+ * The workflow is launched when the user places an order on the app.
+ * There are 2 phases to the workflow - 1. Restaurant phase and 2. Rider phase.
+ * The Rider phase begins when the Restaurant phase reaches completion.
+ */
+object UberEatsExample {
+  type Restaurant = String
+  type Order      = List[(String, Int)]
+  type User       = String
+  type Address    = String
+  type Rider      = String
+
+  sealed trait OrderConfirmationStatus
+
+  object OrderConfirmationStatus {
+
+    case object Confirmed extends OrderConfirmationStatus
+
+    case object Cancelled extends OrderConfirmationStatus
+
+  }
+
+  sealed trait OrderState
+
+  object OrderState {
+
+    case object Waiting extends OrderState
+
+    case object StartedPreparing extends OrderState
+
+    case object FoodPrepared extends OrderState
+
+    case object StartedPacking extends OrderState
+
+    case object FoodPacked extends OrderState
+
+  }
+
+  sealed trait RiderState
+
+  object RiderState {
+
+    case object LookingForRider extends RiderState
+
+    case object RiderAssigned extends RiderState
+
+    case object OutForDelivery extends RiderState
+
+    case object Delivered extends RiderState
+
+  }
+
+  implicit def orderConfirmationStatusSchema: Schema[OrderConfirmationStatus.Confirmed.type] = ???
+
+  lazy val getOrderConfirmationStatus: Activity[(Restaurant, Order), Throwable, OrderConfirmationStatus] =
+    Activity[(Restaurant, Order), Throwable, OrderConfirmationStatus](
+      "get-order-confirmation-status",
+      "Gets whether or not an order is confirmed by the restaurant",
+      ???,
+      ???,
+      ???
+    )
+
+  lazy val restaurantOrderStatus: ZFlow[(Restaurant, Order), Throwable, Unit] = {
+    for {
+      tuple           <- ZFlow.input[(Restaurant, Order)]
+      orderConfStatus <- getOrderConfirmationStatus(tuple)
+      _               <- ZFlow.ifThenElse(orderConfStatus == Expr(OrderConfirmationStatus.Confirmed))(
+                           processOrderWorkflow,
+                           cancelOrderWorkflow
+                         )
+    } yield ()
+  }
+
+  lazy val getOrderState: Activity[(User, Restaurant, Order), Throwable, OrderState]         = ???
+  lazy val pushOrderStatusNotification: Activity[(User, Restaurant, Order), Throwable, Unit] = ???
+  lazy val pushRideStatusNotification: Activity[(User, Address), Throwable, Unit]            = ???
+  lazy val assignRider: Activity[(User, Address), Throwable, Rider]                          = ???
+  lazy val getRiderState: Activity[(User, Address), Throwable, RiderState]                   = ???
+
+  lazy val processOrderWorkflow: ZFlow[(User, Restaurant, Order), Throwable, Any] = {
+    implicit def schemaOrderStateWaiting: Schema[OrderState] = ???
+
+    implicit val sortableOrderState: Sortable[OrderState] = ???
+
+    def updateOrderState(
+      tuple3: Expr[(User, Restaurant, Order)]
+    ): ZFlow[Any, Throwable, OrderState] =
+      ZFlow(OrderState.Waiting: OrderState).iterate((orderState: Expr[OrderState]) =>
+        for {
+          currOrderState <- getOrderState(tuple3)
+          _              <- ZFlow.ifThenElse(currOrderState !== orderState)(pushOrderStatusNotification(tuple3), ZFlow.unit)
+          _              <- ZFlow.sleep(Expr.ofMinutes(2L))
+        } yield currOrderState
+      )(_ !== (OrderState.FoodPacked: OrderState))
+
+    for {
+      tuple3 <- ZFlow.input[(User, Restaurant, Order)]
+      _      <- updateOrderState(tuple3)
+    } yield ()
+  }
+
+  def riderWorkflow(tuple2: Expr[(User, Address)]): ZFlow[Any, Throwable, Any] = {
+    implicit def schemaRiderState: Schema[RiderState] = ???
+
+    implicit val sortableRiderState: Sortable[RiderState] = ???
+
+    def updateRiderState(tuple2: Expr[(Rider, Address)]): ZFlow[Any, Throwable, RiderState] =
+      ZFlow(RiderState.RiderAssigned: RiderState).iterate((riderState: Expr[RiderState]) =>
+        for {
+          currRiderState <- getRiderState(tuple2)
+          _              <- ZFlow.ifThenElse(currRiderState !== riderState)(pushRideStatusNotification(tuple2), ZFlow.unit)
+          _              <- ZFlow.sleep(Expr.ofMinutes(2L))
+        } yield currRiderState
+      )(_ !== Expr(RiderState.Delivered: RiderState))
+
+    for {
+      rider <- assignRider(tuple2)
+      _     <- updateRiderState((rider, tuple2._2))
+    } yield ()
+  }
+
+  lazy val cancelOrderWorkflow: ZFlow[(Restaurant, Order), Throwable, Any] = ???
 }
