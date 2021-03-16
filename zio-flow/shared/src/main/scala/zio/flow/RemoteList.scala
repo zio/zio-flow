00a0c00656b5d5bcc110babb39aa7a2d478b9321
ashprakasan <prakasanaiswarya@gmail.com>
2021-03-17 02:56:01 +0530

    Implement more methods for List

--- zio-flow/shared/src/main/scala/zio/flow/RemoteList.scala
+++ zio-flow/shared/src/main/scala/zio/flow/RemoteList.scala
@@ -5,7 +5,10 @@ import zio.flow.Remote.{ Cons, apply }
 trait RemoteList[+A] {
   def self: Remote[A]
 
-  def ++[A1](other: Remote[List[A1]])(implicit ev: A <:< List[A1]): Remote[List[A1]] = ???
+  def ++[A1](other: Remote[List[A1]])(implicit ev: A <:< List[A1]): Remote[List[A1]] = {
+    val reversedSelf : Remote[List[A1]] = self.reverse
+    reversedSelf.fold[A1, List[A1]](other)((l,a) => Remote.Cons(l,a))
+  }
 
   def reverse[A0](implicit ev: A <:< List[A0]): Remote[List[A0]] =
     fold[A0, List[A0]](Remote(Nil))((l, a) => Remote.Cons(l, a))
@@ -28,9 +31,18 @@ trait RemoteList[+A] {
           predicate(tuple._1).ifThenElse(Cons(tuple._2.takeWhile[A1](predicate), tuple._1), Nil)
       )
 
-  def drop(num: Remote[Int]): Remote[List[A]] = ???
+  def drop[A1](num: Remote[Int])(implicit ev: A <:< List[A1]): Remote[List[A1]] = {
+    val ifTrue = Remote
+      .UnCons(self.widen[List[A1]])
+      .widen[Option[(A1, List[A1])]]
+      .option(Nil, (tuple: Remote[(A1, List[A1])]) => tuple._2.drop[A1](num - Remote(1)))
 
-  def dropWhile(predicate: Remote[A] => Remote[Boolean]): Remote[List[A]] = ???
+    (num > Remote(0)).ifThenElse(ifTrue, self.widen[List[A1]])
+  }
+
+  def dropWhile[A1](predicate: Remote[A1] => Remote[Boolean])(implicit ev : A<:< List[A1]): Remote[List[A1]] = {
+    Remote.UnCons(self.widen[List[A1]]).widen[Option[(A1, List[A1])]].option(Nil, (tuple: Remote[(A1, List[A1])]) => tuple._2.dropWhile[A1](predicate))
+  }
 
   final def fold[A0, B](initial: Remote[B])(f: (Remote[B], Remote[A0]) => Remote[B])(implicit
     ev: A <:< List[A0]
