3fdbeccaa48f51b5bf6d02a246a98689b8e00856
Ash Prakasan <prakasanaiswarya@gmail.com>
2021-03-30 12:00:14 +0530

    Changes to NumericSpec.

--- zio-flow/shared/src/test/scala/zio/flow/NumericSpec.scala
+++ zio-flow/shared/src/test/scala/zio/flow/NumericSpec.scala
@@ -1,222 +1,167 @@
 package zio.flow
 
+import zio.flow.NumericSpec.root
 import zio.flow.Remote.apply
 import zio.test.Assertion.equalTo
 import zio.test.{DefaultRunnableSpec, ZSpec, _}
 
 object NumericSpec extends DefaultRunnableSpec {
 
-  val suite1 = suite("NumericAdditionSpec")(test("Test addition of Remote Integer.") {
-    val fInt = Remote(1)
-    val sInt = Remote(2)
-    assert((fInt + sInt).eval)(equalTo(Right(3)))
-  },
-    test("Test addition of Remote Long") {
-      val fLong = Remote(1L)
-      val sLong = Remote(2L)
-      assert((fLong + sLong).eval)(equalTo(Right(3L)))
-    },
-    test("Test addition of Remote Short") {
-      val fShort = Remote(1.toShort)
-      val sShort = Remote(2.toShort)
-      assert((fShort + sShort).eval)(equalTo(Right(3.toShort)))
-    },
-    test("Test addition of Remote Float") {
-      val fFloat = Remote(1.555f)
-      val sFloat = Remote(2.333f)
-      assert((fFloat + sFloat).eval)(equalTo(Right(3.888f)))
-    },
-    test("Test addition of Remote Double") {
-      val fDouble = Remote(1.555657)
-      val sDouble = Remote(2.333665)
-      assert((fDouble + sDouble).eval)(equalTo(Right(3.889322)))
-    },
-    test("Test addition of BigInt"){
-      val fBigInt = BigInt("111111111111111")
-      val sBigInt = BigInt("222222222222222")
-      assert((fBigInt + sBigInt).eval)(equalTo(Right(BigInt("333333333333333"))))
-    },
-    test("Test addition of BigDecimal"){
-      val fBigDecimal = BigDecimal("11111.1111111111")
-      val sBigDecimal = BigDecimal("22222.2222222222")
-      assert((fBigDecimal + sBigDecimal).eval)(equalTo(Right(BigDecimal("33333.3333333333"))))
+  private def addition[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) = {
+    test("Test Addition") {
+      assert((Remote(first) + Remote(second)).eval)(equalTo(Right(expected)))
     }
+  }
+
+  private def subtraction[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) = {
+    test("Test Subtraction") {
+      assert((Remote(first) - Remote(second)).eval)(equalTo(Right(expected)))
+    }
+  }
+
+  private def multiplication[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) = {
+    test("Test Multiplication") {
+      assert((Remote(first) * Remote(second)).eval)(equalTo(Right(expected)))
+    }
+  }
+
+  private def division[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) = {
+    test("Test Division") {
+      assert((Remote(first) / Remote(second)).eval)(equalTo(Right(expected)))
+    }
+  }
+
+  private def log[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) = {
+    test("Test Log") {
+      assert((Remote(first) log Remote(second)).eval)(equalTo(Right(expected)))
+    }
+  }
+
+  private def root[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) = {
+    test("Test Log") {
+      assert((Remote(first) root Remote(second)).eval)(equalTo(Right(expected)))
+    }
+  }
+
+  val suite1 = suite("NumericIntSpec")(
+    addition(1, 2, 1 + 2),
+    subtraction(1, 2, -1),
+    multiplication(1, 2, 2),
+    division(1, 2, 0),
+    log(1, 2, Math.log(1) / Math.log(2)),
+    root(1L, 2L, 1L)
   )
 
-  val suite2 = suite("NumericSubtractionSpec")(test("Test subtraction of Remote Numerics.") {
-    val fInt = Remote(1)
-    val sInt = Remote(2)
-    assert((fInt - sInt).eval)(equalTo(Right(-1)))
-  },
-    test("Test subtraction of Remote Long") {
-      val fLong = Remote(1L)
-      val sLong = Remote(2L)
-      assert((fLong - sLong).eval)(equalTo(Right(-1L)))
-    },
-    test("Test subtraction of Remote Short") {
-      val fShort = Remote(1.toShort)
-      val sShort = Remote(2.toShort)
-      assert((fShort - sShort).eval)(equalTo(Right((-1).toShort)))
-    },
-    test("Test subtraction of Remote Float") {
-      val fFloat = Remote(1.555f)
-      val sFloat = Remote(2.333f)
-      assert((fFloat - sFloat).eval)(equalTo(Right(-0.778f)))
-    },
-    test("Test subtraction of Remote Double") {
-      val fDouble = Remote(1.555657)
-      val sDouble = Remote(2.333665)
-      assert((fDouble - sDouble).eval)(equalTo(Right(-0.7780079999999998)))
-    },
-    test("Test subtraction of BigInt"){
-      val fBigInt = BigInt("111111111111111")
-      val sBigInt = BigInt("222222222222222")
-      assert((fBigInt - sBigInt).eval)(equalTo(Right(BigInt("-111111111111111"))))
-    },
-    test("Test subtraction of BigDecimal"){
-      val fBigDecimal = BigDecimal("11111.1111111111")
-      val sBigDecimal = BigDecimal("22222.2222222222")
-      assert((fBigDecimal - sBigDecimal).eval)(equalTo(Right(BigDecimal("-11111.1111111111"))))
-    })
+  val suite2 = suite("NumericLongSpec")(
+    addition(1L, 2L, 1L + 2L),
+    subtraction(1L, 2L, -1L),
+    multiplication(1L, 2L, 2L),
+    division(1L, 2L, 0L),
+    log(1L, 2L, Math.log(1L) / Math.log(2L)),
+    root(1L, 2L, 1L)
+  )
 
-  val suite3 = suite("NumericMultiplicationSpec")(test("Test multiplication of Remote Numerics.") {
-    val fInt: Remote[Int] = Remote(1)
-    val sInt = Remote(2)
-    assert((fInt * sInt).eval)(equalTo(Right(2)))
-  },
-    test("Test multiplication of Remote Long") {
-      val fLong = Remote(1L)
-      val sLong = Remote(2L)
-      assert((fLong * sLong).eval)(equalTo(Right(2L)))
-    },
-    test("Test multiplication of Remote Short") {
-      val fShort = Remote(1.toShort)
-      val sShort = Remote(2.toShort)
-      assert((fShort * sShort).eval)(equalTo(Right(2.toShort)))
-    },
-    test("Test multiplication of Remote Float") {
-      val fFloat = Remote(1.555f)
-      val sFloat = Remote(2.333f)
-      assert((fFloat * sFloat).eval)(equalTo(Right(3.6278148f)))
-    },
-    test("Test multiplication of Remote Double") {
-      val fDouble = Remote(1.555657)
-      val sDouble = Remote(2.333665)
-      assert((fDouble * sDouble).eval)(equalTo(Right(3.630382292905)))
-    },
-    test("Test multiplication of BigInt"){
-      val fBigInt = BigInt("111111111111111")
-      val sBigInt = BigInt("222222222222222")
-      assert((fBigInt * sBigInt).eval)(equalTo(Right(BigInt("24691358024691308641975308642"))))
-    },
-    test("Test subtraction of BigDecimal"){
-      val fBigDecimal = BigDecimal("11111.1111111111")
-      val sBigDecimal = BigDecimal("22222.2222222222")
-      assert((fBigDecimal * sBigDecimal).eval)(equalTo(Right(BigDecimal("246913580.24691308641975308642"))))
-    })
+  val suite3 = suite("NumericShortSpec")(
+    addition(1.toShort, 2.toShort, 1.toShort + 2.toShort),
+    subtraction(1.toShort, 2.toShort, 1.toShort - 2.toShort),
+    multiplication(1.toShort, 2.toShort, 1.toShort * 2.toShort),
+    division(1.toShort, 2.toShort, 1.toShort / 2.toShort),
+    log(1.toShort, 2.toShort, Math.log(1.toShort) / Math.log(2.toShort)),
+    root(1.toShort, 2.toShort, 1.toShort)
+  )
 
-  val suite4 = suite("NumericDivisionSpec")(test("Test division of Remote Numerics.") {
-    val fInt: Remote[Int] = Remote(1)
-    val sInt: Remote[Int] = Remote(2)
-    assert((fInt / sInt).eval)(equalTo(Right(0)))
-  },
-    test("Test division of Remote Long") {
-      val fLong = Remote(1L)
-      val sLong = Remote(2L)
-      assert((fLong / sLong).eval)(equalTo(Right(0L)))
-    },
-    test("Test division of Remote Short") {
-      val fShort = Remote(1.toShort)
-      val sShort = Remote(2.toShort)
-      assert((fShort / sShort).eval)(equalTo(Right(0.toShort)))
-    },
-    test("Test division of Remote Float") {
-      val fFloat = Remote(1.555f)
-      val sFloat = Remote(2.333f)
-      assert((fFloat / sFloat).eval)(equalTo(Right(0.66652375f)))
-    },
-    test("Test division of Remote Double") {
-      val fDouble = Remote(1.555657)
-      val sDouble = Remote(2.333665)
-      assert((fDouble / sDouble).eval)(equalTo(Right(0.6666153882412429)))
-    },
-    test("Test division of BigInt"){
-      val fBigInt = BigInt("111111111111111")
-      val sBigInt = BigInt("222222222222222")
-      assert((fBigInt / sBigInt).eval)(equalTo(Right(BigInt("0"))))
-    },
-    test("Test subtraction of BigDecimal"){
-      val fBigDecimal = BigDecimal("11111.1111111111")
-      val sBigDecimal = BigDecimal("22222.2222222222")
-      assert((fBigDecimal / sBigDecimal).eval)(equalTo(Right(BigDecimal("0.5"))))
-    })
+  val suite4 = suite("NumericFloatSpec")(
+    addition(1.555f, 2.333f, 1.555f + 2.333f),
+    subtraction(1.555f, 2.333f, 1.555f - 2.333f),
+    multiplication(1.555f, 2.333f, 1.555f * 2.333f),
+    division(1.555f, 2.333f, 1.555f / 2.333f),
+    log(1.555f, 2.333f, Math.log(1.555f) / Math.log(2.333f)),
+    root(1.555f, 2.333f, 1.2083198f)
+  )
 
-  val suite5 = suite("NumericLogSpec")(test("Test log of Remote Numerics.") {
-    val fInt = Remote(1)
-    val sInt = Remote(2)
-    assert((fInt log sInt).eval)(equalTo(Right(0)))
-  },
-    test("Test log of Remote Long") {
-      val fLong = Remote(1L)
-      val sLong = Remote(2L)
-      assert((fLong log sLong).eval)(equalTo(Right(0L)))
-    },
-    test("Test log of Remote Short") {
-      val fShort = Remote(1.toShort)
-      val sShort = Remote(2.toShort)
-      assert((fShort log sShort).eval)(equalTo(Right((0L).toShort)))
-    },
-    test("Test log of Remote Float") {
-      val fFloat = Remote(1.555f)
-      val sFloat = Remote(2.333f)
-      assert((fFloat log sFloat).eval)(equalTo(Right(0.5211272f)))
-    },
-    test("Test log of Remote Double") {
-      val fDouble = Remote(1.555657)
-      val sDouble = Remote(2.333665)
-      assert((fDouble log sDouble).eval)(equalTo(Right(0.5214504484258191)))
-    },
-    test("Test log of BigInt"){
-      val fBigInt = BigInt("111111111111111")
-      val sBigInt = BigInt("222222222222222")
-      assert((fBigInt log sBigInt).eval)(equalTo(Right(BigInt("0"))))
-    },
-    test("Test log of BigDecimal"){
-      val fBigDecimal = BigDecimal("11111.1111111111")
-      val sBigDecimal = BigDecimal("22222.2222222222")
-      assert((fBigDecimal log sBigDecimal).eval)(equalTo(Right(BigDecimal("0.9307465578618759"))))
-    })
+  val suite5 = suite("NumericDoubleSpec")(
+    addition(1.332435, 2.232123, 1.332435 + 2.232123),
+    subtraction(1.555657, 2.333665, 1.555657 - 2.333665),
+    multiplication(1.555657, 2.333665, 1.555657 * 2.333665),
+    division(1.555657, 2.333665, 1.555657 / 2.333665),
+    log(1.555657, 2.333665, Math.log(1.555657) / Math.log(2.333665)),
+    root(1.555657, 2.333665, 1.2084734191706394)
+  )
 
-  val suite6 =  suite("NumericLogSpec")(test("Test root of Remote Long") {
-    val fLong = Remote(1L)
-    val sLong = Remote(2L)
-    assert((fLong root sLong).eval)(equalTo(Right(1L)))
-  },
-  test("Test root of Remote Short") {
-    val fShort = Remote(1.toShort)
-    val sShort = Remote(2.toShort)
-    assert((fShort root sShort).eval)(equalTo(Right((1L).toShort)))
-  },
-  test("Test root of Remote Float") {
-    val fFloat = Remote(1.555f)
-    val sFloat = Remote(2.333f)
-    assert((fFloat root sFloat).eval)(equalTo(Right(1.2083198f)))
-  },
-  test("Test root of Remote Double") {
-    val fDouble = Remote(1.555657)
-    val sDouble = Remote(2.333665)
-    assert((fDouble root sDouble).eval)(equalTo(Right(1.2084734191706394)))
-  },
-  test("Test root of BigInt"){
-    val fBigInt = BigInt("111111111111111")
-    val sBigInt = BigInt("222222222222222")
-    assert((fBigInt root sBigInt).eval)(equalTo(Right(BigInt("1"))))
-  },
-  test("Test root of BigDecimal"){
-    val fBigDecimal = BigDecimal("11111.1111111111")
-    val sBigDecimal = BigDecimal("22222.2222222222")
-    assert((fBigDecimal root sBigDecimal).eval)(equalTo(Right(BigDecimal("1.0004192944192845"))))
-  })
+  val suite6 = suite("NumericBigIntSpec")(
+    addition(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") + BigInt("22222222222")),
+    subtraction(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") - BigInt("22222222222")),
+    multiplication(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") * BigInt("22222222222")),
+    division(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") / BigInt("22222222222")),
+    log(BigInt("11111111111"), BigInt("22222222222"), BigInt((Math.log(BigInt("11111111111").doubleValue) / Math.log(BigInt("22222222222").doubleValue)).toInt)),
+    root(BigInt("111111111111111"), BigInt("222222222222222"), BigInt("1"))
+  )
+
+  val suite7 = suite("NumericBigDecimalSpec")(
+    addition(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") + BigDecimal("222222.222222")),
+    subtraction(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") - BigDecimal("222222.222222")),
+    multiplication(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") * BigDecimal("222222.222222")),
+    division(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") / BigDecimal("222222.222222")),
+    log(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal(Math.log(BigDecimal("111111.111111").doubleValue) / Math.log(BigDecimal("222222.222222").doubleValue))),
+    root(BigDecimal("11111.1111111111"), BigDecimal("22222.2222222222"), BigDecimal("1.0004192944192845"))
+  )
+//  val suite1 = suite("NumericAdditionSpec")(addition(1, 2, 1 + 2),
+//    addition(1L, 2L, 1L + 2L),
+//    addition(1.toShort, 2.toShort, 1.toShort + 2.toShort),
+//    addition(1.555f, 2.333f, 1.555f + 2.333f),
+//    addition(1.332435, 2.232123, 1.332435 + 2.232123),
+//    addition(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") + BigInt("22222222222")),
+//    addition(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") + BigDecimal("222222.222222")))
+//
+//  val suite2 = suite("NumericSubtractionSpec")(
+//    subtraction(1, 2, -1),
+//    subtraction(1L, 2L, -1L),
+//    subtraction(1.toShort, 2.toShort, 1.toShort - 2.toShort),
+//    subtraction(1.555f, 2.333f, 1.555f - 2.333f),
+//    subtraction(1.555657, 2.333665, 1.555657 - 2.333665),
+//    subtraction(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") - BigInt("22222222222")),
+//    subtraction(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") - BigDecimal("222222.222222"))
+//  )
+//
+//  val suite3 = suite("NumericMultiplicationSpec")(
+//    multiplication(1, 2, 2),
+//    multiplication(1L, 2L, 2L),
+//    multiplication(1.toShort, 2.toShort, 1.toShort * 2.toShort),
+//    multiplication(1.555f, 2.333f, 1.555f * 2.333f),
+//    multiplication(1.555657, 2.333665, 1.555657 * 2.333665),
+//    multiplication(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") * BigInt("22222222222")),
+//    multiplication(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") * BigDecimal("222222.222222"))
+//  )
+//
+//  val suite4 = suite("NumericDivisionSpec")(
+//    division(1, 2, 0),
+//    division(1L, 2L, 0L),
+//    division(1.toShort, 2.toShort, 1.toShort / 2.toShort),
+//    division(1.555f, 2.333f, 1.555f / 2.333f),
+//    division(1.555657, 2.333665, 1.555657 / 2.333665),
+//    division(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") / BigInt("22222222222")),
+//    division(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal("111111.111111") / BigDecimal("222222.222222"))
+//  )
+//
+//  val suite5 = suite("NumericLogSpec")(
+//    log(1, 2, Math.log(1) / Math.log(2)),
+//    log(1L, 2L, Math.log(1L) / Math.log(2L)),
+//    log(1.toShort, 2.toShort, Math.log(1.toShort) / Math.log(2.toShort)),
+//    log(1.555f, 2.333f, Math.log(1.555f) / Math.log(2.333f)),
+//    log(1.555657, 2.333665, Math.log(1.555657) / Math.log(2.333665)),
+//    log(BigInt("11111111111"), BigInt("22222222222"), BigInt((Math.log(BigInt("11111111111").doubleValue) / Math.log(BigInt("22222222222").doubleValue)).toInt)),
+//    log(BigDecimal("111111.111111"), BigDecimal("222222.222222"), BigDecimal(Math.log(BigDecimal("111111.111111").doubleValue) / Math.log(BigDecimal("222222.222222").doubleValue)))
+//  )
+//
+//  val suite6 = suite("NumericRootSpec")(
+//    root(1L, 2L, 1L),
+//    root(1.toShort, 2.toShort, 1.toShort),
+//    root(1.555f, 2.333f, 1.2083198f),
+//    root(1.555657, 2.333665, 1.2084734191706394),
+//    root(BigInt("111111111111111"), BigInt("222222222222222"), BigInt("1")),
+//    root(BigDecimal("11111.1111111111"), BigDecimal("22222.2222222222"), BigDecimal("1.0004192944192845"))
+  //)
 
   override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] = suite("NumericSpec")(suite1, suite2, suite3, suite4, suite5, suite6)
 }
+
