0777c53d97728fea6713003ee368adf986356ddc
Ash Prakasan <prakasanaiswarya@gmail.com>
2021-04-02 02:55:07 +0530

    Remove laziness from ifThenElse, other small changes.

--- zio-flow/shared/src/main/scala/zio/flow/Tuples.scala
+++
@@ -1,7 +0,0 @@
-package zio.flow
-
-object Tuples {
-  // TODO: Path-dependent types for all arities
-  def unapply[A, B](remote: Remote[(A, B)]): Some[(Remote[A], Remote[B])] =
-    Some((remote._1, remote._2))
-}
