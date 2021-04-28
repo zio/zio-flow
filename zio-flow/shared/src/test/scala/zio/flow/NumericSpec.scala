2990f375f67de414e25985cdb39e2e15669e5c37
Kit Langton <kit.langton@gmail.com>
2021-04-28 14:21:39 -0700

    refactor NumericSpec to use property tests

--- zio-flow/shared/src/test/scala/zio/flow/NumericSpec.scala
+++ zio-flow/shared/src/test/scala/zio/flow/NumericSpec.scala
@@ -1,130 +1,149 @@
 package zio.flow
 
-import zio.test.Assertion.equalTo
-import zio.test.{ DefaultRunnableSpec, ZSpec, _ }
+import zio.flow.utils.RemoteAssertionSyntax.RemoteAssertionOps
+import zio.random.Random
+import zio.test._
 
 object NumericSpec extends DefaultRunnableSpec {
-  val first: Short  = 1
-  val second: Short = 2
-  val sum: Int      = first + second
-
-  private def addition[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) =
-    test("Test Addition") {
-      assert((Remote(first) + Remote(second)).eval)(equalTo(Right(expected)))
-    }
-
-  private def subtraction[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) =
-    test("Test Subtraction") {
-      assert((Remote(first) - Remote(second)).eval)(equalTo(Right(expected)))
-    }
-
-  private def multiplication[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) =
-    test("Test Multiplication") {
-      assert((Remote(first) * Remote(second)).eval)(equalTo(Right(expected)))
-    }
-
-  private def division[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) =
-    test("Test Division") {
-      assert((Remote(first) / Remote(second)).eval)(equalTo(Right(expected)))
-    }
-
-  private def log[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) =
-    test("Test Log") {
-      assert((Remote(first) log Remote(second)).eval)(equalTo(Right(expected)))
-    }
-
-  private def root[T](first: T, second: T, expected: T)(implicit numericT: Numeric[T], schemaT: Schema[T]) =
-    test("Test Log") {
-      assert((Remote(first) root Remote(second)).eval)(equalTo(Right(expected)))
-    }
-
-  val suite1: Spec[Any, TestFailure[Nothing], TestSuccess] = suite("NumericIntSpec")(
-    addition(1, 2, 1 + 2),
-    subtraction(1, 2, -1),
-    multiplication(1, 2, 2),
-    division(1, 2, 0),
-    log(1, 2, Math.log(1) / Math.log(2)),
-    root(1L, 2L, 1L)
-  )
-
-  val suite2: Spec[Any, TestFailure[Nothing], TestSuccess] = suite("NumericLongSpec")(
-    addition(1L, 2L, 1L + 2L),
-    subtraction(1L, 2L, -1L),
-    multiplication(1L, 2L, 2L),
-    division(1L, 2L, 0L),
-    log(1L, 2L, Math.log(1L) / Math.log(2L)),
-    root(1L, 2L, 1L)
-  )
-
-  val suite3: Spec[Any, TestFailure[Nothing], TestSuccess] = suite("NumericShortSpec")(
-    addition(first, second, (first + second).toShort),
-    subtraction(first, second, (first - second).toShort),
-    multiplication(first, second, (first * second).toShort),
-    division(first, second, (first / second).toShort),
-    log(first, second, (Math.log(first.toDouble) / Math.log(second.toDouble)).toShort),
-    root(first, second, first.toShort)
-  )
-
-  val suite4: Spec[Any, TestFailure[Nothing], TestSuccess] = suite("NumericFloatSpec")(
-    addition(1.555f, 2.333f, 1.555f + 2.333f),
-    subtraction(1.555f, 2.333f, 1.555f - 2.333f),
-    multiplication(1.555f, 2.333f, 1.555f * 2.333f),
-    division(1.555f, 2.333f, 1.555f / 2.333f),
-    log(1.555f, 2.333f, Math.log(1.555f) / Math.log(2.333f)),
-    root(1.555f, 2.333f, 1.2083198f)
-  )
-
-  val suite5: Spec[Any, TestFailure[Nothing], TestSuccess] = suite("NumericDoubleSpec")(
-    addition(1.332435, 2.232123, 1.332435 + 2.232123),
-    subtraction(1.555657, 2.333665, 1.555657 - 2.333665),
-    multiplication(1.555657, 2.333665, 1.555657 * 2.333665),
-    division(1.555657, 2.333665, 1.555657 / 2.333665),
-    log(1.555657, 2.333665, Math.log(1.555657) / Math.log(2.333665)),
-    root(1.555657, 2.333665, 1.2084734191706394)
-  )
-
-  val suite6: Spec[Any, TestFailure[Nothing], TestSuccess] = suite("NumericBigIntSpec")(
-    addition(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") + BigInt("22222222222")),
-    subtraction(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") - BigInt("22222222222")),
-    multiplication(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") * BigInt("22222222222")),
-    division(BigInt("11111111111"), BigInt("22222222222"), BigInt("11111111111") / BigInt("22222222222")),
-    log(
-      BigInt("11111111111"),
-      BigInt("22222222222"),
-      BigInt((Math.log(BigInt("11111111111").doubleValue) / Math.log(BigInt("22222222222").doubleValue)).toInt)
-    ),
-    root(BigInt("111111111111111"), BigInt("222222222222222"), BigInt("1"))
-  )
-
-  val suite7: Spec[Any, TestFailure[Nothing], TestSuccess] = suite("NumericBigDecimalSpec")(
-    addition(
-      BigDecimal("111111.111111"),
-      BigDecimal("222222.222222"),
-      BigDecimal("111111.111111") + BigDecimal("222222.222222")
-    ),
-    subtraction(
-      BigDecimal("111111.111111"),
-      BigDecimal("222222.222222"),
-      BigDecimal("111111.111111") - BigDecimal("222222.222222")
-    ),
-    multiplication(
-      BigDecimal("111111.111111"),
-      BigDecimal("222222.222222"),
-      BigDecimal("111111.111111") * BigDecimal("222222.222222")
-    ),
-    division(
-      BigDecimal("111111.111111"),
-      BigDecimal("222222.222222"),
-      BigDecimal("111111.111111") / BigDecimal("222222.222222")
-    ),
-    log(
-      BigDecimal("111111.111111"),
-      BigDecimal("222222.222222"),
-      BigDecimal(Math.log(BigDecimal("111111.111111").doubleValue) / Math.log(BigDecimal("222222.222222").doubleValue))
-    ),
-    root(BigDecimal("11111.1111111111"), BigDecimal("22222.2222222222"), BigDecimal("1.0004192944192845"))
-  )
 
   override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] =
-    suite("NumericSpec")(suite1, suite2, suite3, suite4, suite5, suite6)
+    suite("NumericSpec")(
+      numericTests("Int", Gen.anyInt)(Operations.intOperations),
+      numericTests("Long", Gen.anyLong)(Operations.longOperations),
+      numericTests("Short", Gen.anyShort)(Operations.shortOperations),
+      numericTests("Float", Gen.anyFloat)(Operations.floatOperations),
+      numericTests("Double", Gen.anyDouble)(Operations.doubleOperations),
+      numericTests("BigInt", Gen.bigInt(BigInt(Int.MinValue), BigInt(Int.MaxValue)))(Operations.bigIntOperations),
+      bigDecimalNumericSuite
+    )
+
+  // Not working.
+  val bigDecimalNumericSuite: Spec[Random with TestConfig, TestFailure[Nothing], TestSuccess] =
+    numericTestsWithoutLogOrRoot(
+      "BigDecimal",
+      Gen.bigDecimal(BigDecimal(Double.MinValue), BigDecimal(Double.MaxValue))
+    )(
+      Operations.bigDecimalOperations
+    )
+
+  def numericTests[R, A: Schema](name: String, gen: Gen[R, A])(ops: Operations[A])(implicit
+    numericT: Numeric[A]
+  ): Spec[R with TestConfig, TestFailure[Nothing], TestSuccess] =
+    suite(name)(
+      testOp[R, A]("Addition", gen, gen)(_ + _)(ops.addition),
+      testOp[R, A]("Subtraction", gen, gen)(_ - _)(ops.subtraction),
+      testOp[R, A]("Multiplication", gen, gen)(_ * _)(ops.multiplication),
+      testOp[R, A]("Division", gen, gen.filterNot(ops.isZero))(_ / _)(ops.division),
+      testOp[R, A]("Log", gen, gen)(_ log _)(ops.log),
+      testOp[R, A]("Root", gen, gen)(_ root _)(ops.root)
+    )
+
+  // TODO: BigDecimal fails Log/Root specs
+  def numericTestsWithoutLogOrRoot[R, A: Schema](name: String, gen: Gen[R, A])(ops: Operations[A])(implicit
+    numericT: Numeric[A]
+  ): Spec[R with TestConfig, TestFailure[Nothing], TestSuccess] =
+    suite(name)(
+      testOp[R, A]("Addition", gen, gen)(_ + _)(ops.addition),
+      testOp[R, A]("Subtraction", gen, gen)(_ - _)(ops.subtraction),
+      testOp[R, A]("Multiplication", gen, gen)(_ * _)(ops.multiplication),
+      testOp[R, A]("Division", gen, gen.filterNot(ops.isZero))(_ / _)(ops.division)
+//      testOp[R, A]("Log", gen, gen)(_ log _)(ops.log),
+//      testOp[R, A]("Root", gen, gen)(_ root _)(ops.root)
+    )
+
+  private def testOp[R, A: Schema: Numeric](name: String, genX: Gen[R, A], genY: Gen[R, A])(
+    numericOp: (Remote[A], Remote[A]) => Remote[A]
+  )(op: (A, A) => A): ZSpec[R with TestConfig, Nothing] =
+    testM(name) {
+      check(genX, genY) { case (x, y) =>
+        numericOp(x, y) <-> op(x, y)
+      }
+    }
+
+  private case class Operations[A](
+    addition: (A, A) => A,
+    subtraction: (A, A) => A,
+    multiplication: (A, A) => A,
+    division: (A, A) => A,
+    isZero: A => Boolean,
+    log: (A, A) => A,
+    root: (A, A) => A
+  )
+
+  private object Operations {
+    val intOperations: Operations[Int] =
+      Operations[Int](
+        addition = _ + _,
+        subtraction = _ - _,
+        multiplication = _ * _,
+        division = _ / _,
+        isZero = _ == 0,
+        log = (x, y) => (Math.log(x.toDouble) / Math.log(y.toDouble)).toInt,
+        root = (x, y) => Math.pow(x.toDouble, 1 / y.toDouble).toInt
+      )
+
+    val bigIntOperations: Operations[BigInt] =
+      Operations[BigInt](
+        addition = _ + _,
+        subtraction = _ - _,
+        multiplication = _ * _,
+        division = _ / _,
+        isZero = _ == 0,
+        log = (x, y) => (Math.log(x.doubleValue) / Math.log(y.doubleValue)).toInt,
+        root = (x, y) => Math.pow(x.toDouble, 1 / y.toDouble).toInt
+      )
+
+    val bigDecimalOperations: Operations[BigDecimal] =
+      Operations[BigDecimal](
+        addition = _ + _,
+        subtraction = _ - _,
+        multiplication = _ * _,
+        division = _ / _,
+        isZero = _ == 0,
+        log = (x, y) => Math.log(x.doubleValue) / Math.log(y.doubleValue),
+        root = (x, y) => Math.pow(x.toDouble, 1 / y.toDouble)
+      )
+
+    val longOperations: Operations[Long] =
+      Operations[Long](
+        addition = _ + _,
+        subtraction = _ - _,
+        multiplication = _ * _,
+        division = _ / _,
+        isZero = _ == 0,
+        log = (x, y) => (Math.log(x.toDouble) / Math.log(y.toDouble)).toLong,
+        root = (x, y) => Math.pow(x.toDouble, 1 / y.toDouble).toLong
+      )
+
+    val shortOperations: Operations[Short] =
+      Operations[Short](
+        addition = (x, y) => (x + y).toShort,
+        subtraction = (x, y) => (x - y).toShort,
+        multiplication = (x, y) => (x * y).toShort,
+        division = (x, y) => (x / y).toShort,
+        isZero = _ == 0,
+        log = (x, y) => (Math.log(x.toDouble) / Math.log(y.toDouble)).toShort,
+        root = (x, y) => Math.pow(x.toDouble, 1 / y.toDouble).toShort
+      )
+
+    val doubleOperations: Operations[Double] = Operations[Double](
+      addition = _ + _,
+      subtraction = _ - _,
+      multiplication = _ * _,
+      division = _ / _,
+      isZero = _ == 0,
+      log = (x, y) => Math.log(x) / Math.log(y),
+      root = (x, y) => Math.pow(x, 1 / y)
+    )
+
+    val floatOperations: Operations[Float] = Operations[Float](
+      addition = _ + _,
+      subtraction = _ - _,
+      multiplication = _ * _,
+      division = _ / _,
+      isZero = _ == 0,
+      log = (x, y) => (Math.log(x.toDouble) / Math.log(y.toDouble)).toFloat,
+      root = (x, y) => Math.pow(x.toDouble, 1 / y.toDouble).toFloat
+    )
+  }
 }
