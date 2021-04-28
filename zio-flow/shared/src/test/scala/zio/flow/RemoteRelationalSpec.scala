d9a5d4786c6662b2c0a38cfb254a0324375c22f1
Kit Langton <kit.langton@gmail.com>
2021-04-28 14:20:54 -0700

    add RemoteRelationalSpec

---
+++ zio-flow/shared/src/test/scala/zio/flow/RemoteRelationalSpec.scala
@@ -0,0 +1,30 @@
+package zio.flow
+
+import zio.flow.utils.RemoteAssertionSyntax.RemoteAssertionOps
+import zio.random.Random
+import zio.test._
+
+object RemoteRelationalSpec extends DefaultRunnableSpec {
+
+  val lessThanIntGen: Gen[Random, (Int, Int)] = for {
+    x <- Gen.anyInt
+    y <- Gen.int(x + 1, Int.MaxValue)
+  } yield (x, y)
+
+  override def spec: ZSpec[TestConfig with Random with Annotations, Any] =
+    suite("RemoteRelationalSpec")(
+      testM("Int") {
+        check(lessThanIntGen) { case (x, y) =>
+          BoolAlgebra.all(
+            Remote(x) < Remote(y) <-> true,
+            Remote(x) <= Remote(y) <-> true,
+            (Remote(x) !== Remote(y)) <-> true,
+            Remote(x) > Remote(y) <-> false,
+            Remote(x) >= Remote(y) <-> false,
+            (Remote(x) === Remote(y)) <-> false
+          )
+        }
+      }
+    ) @@ TestAspect.ignore
+
+}
