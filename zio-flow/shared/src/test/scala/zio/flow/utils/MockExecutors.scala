410a4ccb90ecbe022b4f2280b3fd91e187bf7b07
Adam Fraser <adam.fraser@gmail.com>
2021-12-16 20:07:39 -0800

    implement durable promise

--- zio-flow/shared/src/test/scala/zio/flow/utils/MockExecutors.scala
+++ zio-flow/shared/src/test/scala/zio/flow/utils/MockExecutors.scala
@@ -3,7 +3,7 @@ package zio.flow.utils
 import zio.clock.Clock
 import zio.console.Console
 import zio.flow.OperationExecutor
-import zio.flow.internal.PersistentExecutor
+import zio.flow.internal.{DurableLog, PersistentExecutor}
 import zio.flow.internal.ZFlowExecutor.InMemory
 import zio.flow.utils.MockHelpers._
 import zio.{Has, Ref, ZIO}
@@ -23,26 +23,28 @@ object MockExecutors {
       .make[Map[String, Ref[InMemory.State]]](Map.empty)
       .map(ref => InMemory(Has(zio.clock.Clock.Service.live) ++ Has(zio.console.Console.Service.live), mockOpExec, ref))
 
-  val mockPersistentLiveClock: ZIO[Any, Nothing, PersistentExecutor] = Ref
-    .make[Map[String, Ref[PersistentExecutor.State[_, _]]]](Map.empty)
-    .map(ref =>
-      PersistentExecutor(
-        Clock.Service.live,
-        doesNothingDurableLog,
-        doesNothingKVStore,
-        mockOpExec.asInstanceOf[OperationExecutor[Any]],
-        ref
-      )
+  val mockPersistentLiveClock: ZIO[Has[DurableLog], Nothing, PersistentExecutor] =
+    for {
+      durableLog <- ZIO.service[DurableLog]
+      ref        <- Ref.make[Map[String, Ref[PersistentExecutor.State[_, _]]]](Map.empty)
+    } yield PersistentExecutor(
+      Clock.Service.live,
+      durableLog,
+      doesNothingKVStore,
+      mockOpExec.asInstanceOf[OperationExecutor[Any]],
+      ref
     )
 
-  val mockPersistentTestClock = (for {
-    clock <- ZIO.service[Clock.Service]
-    ref   <- Ref.make[Map[String, Ref[PersistentExecutor.State[_, _]]]](Map.empty)
-  } yield PersistentExecutor(
-    clock,
-    doesNothingDurableLog,
-    doesNothingKVStore,
-    mockOpExec.asInstanceOf[OperationExecutor[Any]],
-    ref
-  ))
+  val mockPersistentTestClock =
+    for {
+      durableLog <- ZIO.service[DurableLog]
+      clock      <- ZIO.service[Clock.Service]
+      ref        <- Ref.make[Map[String, Ref[PersistentExecutor.State[_, _]]]](Map.empty)
+    } yield PersistentExecutor(
+      clock,
+      durableLog,
+      doesNothingKVStore,
+      mockOpExec.asInstanceOf[OperationExecutor[Any]],
+      ref
+    )
 }
