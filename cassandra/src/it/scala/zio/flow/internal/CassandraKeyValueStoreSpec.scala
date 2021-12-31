b0bc7f3c8f1ab5e520388353fc6e2511c5ff00bc
Terry Lin <tlin.gta@gmail.com>
2021-12-31 14:12:11 -0500

    Lowered the amount of inserts in `scanAll` test to just enough to trigger paging.

--- cassandra/src/it/scala/zio/flow/internal/CassandraKeyValueStoreSpec.scala
+++ cassandra/src/it/scala/zio/flow/internal/CassandraKeyValueStoreSpec.scala
@@ -104,7 +104,7 @@ object CassandraKeyValueStoreSpec extends DefaultRunnableSpec {
         val uniqueTableName = newTimeBasedName()
         val keyValuePairs =
           Chunk
-            .fromIterable(1 to 10_000)
+            .fromIterable(1 to 5_001)
             .map { n =>
               Chunk.fromArray(s"abc_$n".getBytes) -> Chunk.fromArray(s"xyz_$n".getBytes)
             }
