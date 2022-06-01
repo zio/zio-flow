aa8d69c05b3aa63198410726a48f8e6d90fa2c4c
Albert Pető <petoalbert32@gmail.com>
2022-06-01 12:07:47 +0200

    Support timestamps in CassandraKeyValueStore (#193)
    * Uncomment and format Cassandra kvstore and tests
    
    * Use timestamps in CassandraKeyValueStore
    
    * Fix deletion in CassandraKeyValueStore
    
    * Update CassandraKvStore documentation

--- cassandra/src/it/scala/zio/flow/cassandra/CassandraKeyValueStoreSpec.scala
+++ cassandra/src/it/scala/zio/flow/cassandra/CassandraKeyValueStoreSpec.scala
@@ -1,136 +1,150 @@
-// TODO
-//package zio.flow.cassandra
-//
-//import CassandraTestContainerSupport.{SessionLayer, cassandraV3, cassandraV4, scyllaDb}
-//import zio.flow.internal.KeyValueStore
-//import zio.{Chunk, ZIO}
-//import zio.test.Assertion.hasSameElements
-//import zio.test.TestAspect.{nondeterministic, sequential}
-//import zio.test.{Gen, Spec, assert, assertTrue, checkN, ZIOSpecDefault}
-//
-//object CassandraKeyValueStoreSpec extends ZIOSpecDefault {
-//
-//  private val cqlNameGen =
-//    Gen.alphaNumericStringBounded(
-//      min = 1,
-//      max = 48
-//    )
-//
-//  private val nonEmptyByteChunkGen =
-//    Gen.chunkOf1(Gen.byte)
-//
-//  private val byteChunkGen =
-//    Gen.chunkOf(Gen.byte)
-//
-//  override def spec: Spec[Environment, Any] =
-//    suite("CassandraKeyValueStoreSpec")(
-//      testUsing(cassandraV3, "Cassandra V3"),
-//      testUsing(cassandraV4, "Cassandra V4"),
-//      testUsing(scyllaDb, "ScyllaDB")
-//    )
-//
-//  private def testUsing(database: SessionLayer, label: String) =
-//    suite(label)(
-//      test("should be able to `put` (upsert) a key-value pair and then `get` it back.") {
-//        checkN(10)(
-//          cqlNameGen,
-//          nonEmptyByteChunkGen,
-//          byteChunkGen,
-//          byteChunkGen
-//        ) { (namespace, key, value1, value2) =>
-//          for {
-//            putSucceeded1 <- KeyValueStore.put(namespace, key, value1)
-//            retrieved1    <- KeyValueStore.get(namespace, key)
-//            putSucceeded2 <- KeyValueStore.put(namespace, key, value2)
-//            retrieved2    <- KeyValueStore.get(namespace, key)
-//          } yield assertTrue(
-//            putSucceeded1,
-//            retrieved1.get == value1,
-//            putSucceeded2,
-//            retrieved2.get == value2
-//          )
-//        }
-//      },
-//      test("should return empty result for a `get` call when the namespace does not exist.") {
-//        checkN(10)(
-//          nonEmptyByteChunkGen
-//        ) { key =>
-//          val nonExistentNamespace = newTimeBasedName()
-//
-//          KeyValueStore
-//            .get(nonExistentNamespace, key)
-//            .map { retrieved =>
-//              assertTrue(
-//                retrieved.isEmpty
-//              )
-//            }
-//        }
-//      },
-//      test("should return empty result for a `get` call when the key does not exist.") {
-//        checkN(10)(
-//          cqlNameGen,
-//          nonEmptyByteChunkGen,
-//          byteChunkGen
-//        ) { (namespace, key, value) =>
-//          val nonExistingKey =
-//            Chunk.fromIterable(newTimeBasedName().getBytes)
-//
-//          for {
-//            putSucceeded <- KeyValueStore.put(namespace, key, value)
-//            retrieved    <- KeyValueStore.get(namespace, nonExistingKey)
-//          } yield assertTrue(
-//            retrieved.isEmpty,
-//            putSucceeded
-//          )
-//        }
-//      },
-//      test("should return empty result for a `scanAll` call when the namespace does not exist.") {
-//        val nonExistentNamespace = newTimeBasedName()
-//
-//        KeyValueStore
-//          .scanAll(nonExistentNamespace)
-//          .runCollect
-//          .map { retrieved =>
-//            assertTrue(
-//              retrieved.isEmpty
-//            )
-//          }
-//      },
-//      test("should return all key-value pairs for a `scanAll` call.") {
-//        val uniqueNamespace = newTimeBasedName()
-//        val keyValuePairs =
-//          Chunk
-//            .fromIterable(1 to 5001)
-//            .map { n =>
-//              Chunk.fromArray(s"abc_$n".getBytes) -> Chunk.fromArray(s"xyz_$n".getBytes)
-//            }
-//        val expectedLength = keyValuePairs.length
-//
-//        for {
-//          putSuccesses <-
-//            ZIO
-//              .foreachPar(keyValuePairs) { case (key, value) =>
-//                KeyValueStore.put(uniqueNamespace, key, value)
-//              }
-//          retrieved <-
-//            KeyValueStore
-//              .scanAll(uniqueNamespace)
-//              .runCollect
-//        } yield {
-//          assertTrue(
-//            putSuccesses.length == expectedLength,
-//            putSuccesses.toSet == Set(true),
-//            retrieved.length == expectedLength
-//          ) &&
-//          assert(retrieved)(
-//            hasSameElements(keyValuePairs)
-//          )
-//        }
-//      }
-//    ).provideCustomLayerShared(database >>> CassandraKeyValueStore.layer) @@ nondeterministic @@ sequential
-//
-//  private def newTimeBasedName() =
-//    s"${java.time.Instant.now}"
-//      .replaceAll(":", "_")
-//      .replaceAll(".", "_")
-//}
+package zio.flow.cassandra
+
+import CassandraTestContainerSupport.{SessionLayer, cassandraV3, cassandraV4, scyllaDb}
+import zio.flow.internal.KeyValueStore
+import zio.{Chunk, ZIO}
+import zio.test.Assertion.hasSameElements
+import zio.test.TestAspect.{nondeterministic, sequential}
+import zio.test.{Gen, Spec, assert, assertTrue, checkN, ZIOSpecDefault}
+import zio.flow.internal.Timestamp
+import zio.test.Assertion.isNone
+
+object CassandraKeyValueStoreSpec extends ZIOSpecDefault {
+
+  private val cqlNameGen =
+    Gen.alphaNumericStringBounded(
+      min = 1,
+      max = 48
+    )
+
+  private val nonEmptyByteChunkGen =
+    Gen.chunkOf1(Gen.byte)
+
+  private val byteChunkGen =
+    Gen.chunkOf(Gen.byte)
+
+  override def spec: Spec[Environment, Any] =
+    suite("CassandraKeyValueStoreSpec")(
+      testUsing(cassandraV3, "Cassandra V3"),
+      testUsing(cassandraV4, "Cassandra V4"),
+      testUsing(scyllaDb, "ScyllaDB")
+    )
+
+  private def testUsing(database: SessionLayer, label: String) =
+    suite(label)(
+      test("should be able to `put` (upsert) a key-value pair and then `get` it back.") {
+        checkN(10)(
+          cqlNameGen,
+          nonEmptyByteChunkGen,
+          byteChunkGen,
+          byteChunkGen
+        ) { (namespace, key, value1, value2) =>
+          for {
+            putSucceeded1 <- KeyValueStore.put(namespace, key, value1, Timestamp(1L))
+            putSucceeded2 <- KeyValueStore.put(namespace, key, value2, Timestamp(2L))
+            retrieved1    <- KeyValueStore.getLatest(namespace, key, Some(Timestamp(1L)))
+            retrieved2    <- KeyValueStore.getLatest(namespace, key, Some(Timestamp(2L)))
+          } yield assertTrue(
+            putSucceeded1,
+            retrieved1.get == value1,
+            putSucceeded2,
+            retrieved2.get == value2
+          )
+        }
+      },
+      test("should be able to delete a key-value pair") {
+        checkN(10)(
+          cqlNameGen,
+          nonEmptyByteChunkGen,
+          byteChunkGen
+        ) { (namespace, key, value1) =>
+          for {
+            _      <- KeyValueStore.put(namespace, key, value1, Timestamp(1L))
+            _      <- KeyValueStore.delete(namespace, key)
+            latest <- KeyValueStore.getLatest(namespace, key, Some(Timestamp(1)))
+          } yield assert(latest)(isNone)
+        }
+      },
+      test("should return empty result for a `get` call when the namespace does not exist.") {
+        checkN(10)(
+          nonEmptyByteChunkGen
+        ) { key =>
+          val nonExistentNamespace = newTimeBasedName()
+
+          KeyValueStore
+            .getLatest(nonExistentNamespace, key, before = None)
+            .map { retrieved =>
+              assertTrue(
+                retrieved.isEmpty
+              )
+            }
+        }
+      },
+      test("should return empty result for a `get` call when the key does not exist.") {
+        checkN(10)(
+          cqlNameGen,
+          nonEmptyByteChunkGen,
+          byteChunkGen
+        ) { (namespace, key, value) =>
+          val nonExistingKey =
+            Chunk.fromIterable(newTimeBasedName().getBytes)
+
+          for {
+            putSucceeded <- KeyValueStore.put(namespace, key, value, Timestamp(1L))
+            retrieved    <- KeyValueStore.getLatest(namespace, nonExistingKey, Some(Timestamp(1L)))
+          } yield assertTrue(
+            retrieved.isEmpty,
+            putSucceeded
+          )
+        }
+      },
+      test("should return empty result for a `scanAll` call when the namespace does not exist.") {
+        val nonExistentNamespace = newTimeBasedName()
+
+        KeyValueStore
+          .scanAll(nonExistentNamespace)
+          .runCollect
+          .map { retrieved =>
+            assertTrue(
+              retrieved.isEmpty
+            )
+          }
+      },
+      test("should return all key-value pairs for a `scanAll` call.") {
+        val uniqueNamespace = newTimeBasedName()
+        val keyValuePairs =
+          Chunk
+            .fromIterable(1 to 5001)
+            .map { n =>
+              Chunk.fromArray(s"abc_$n".getBytes) -> Chunk.fromArray(s"xyz_$n".getBytes)
+            }
+        val expectedLength = keyValuePairs.length
+
+        for {
+          putSuccesses <-
+            ZIO
+              .foreachPar(keyValuePairs) { case (key, value) =>
+                KeyValueStore.put(uniqueNamespace, key, value, Timestamp(1L))
+              }
+          retrieved <-
+            KeyValueStore
+              .scanAll(uniqueNamespace)
+              .runCollect
+        } yield {
+          assertTrue(
+            putSuccesses.length == expectedLength,
+            putSuccesses.toSet == Set(true),
+            retrieved.length == expectedLength
+          ) &&
+          assert(retrieved)(
+            hasSameElements(keyValuePairs)
+          )
+        }
+      }
+    ).provideCustomLayerShared(database >>> CassandraKeyValueStore.layer) @@ nondeterministic @@ sequential
+
+  private def newTimeBasedName() =
+    s"${java.time.Instant.now}"
+      .replaceAll(":", "_")
+      .replaceAll(".", "_")
+}
--- cassandra/src/main/scala/zio/flow/cassandra/CassandraKeyValueStore.scala
+++ cassandra/src/main/scala/zio/flow/cassandra/CassandraKeyValueStore.scala
@@ -1,224 +1,306 @@
-// TODO
-//package zio.flow.cassandra
-//
-//import com.datastax.oss.driver.api.core.cql._
-//import com.datastax.oss.driver.api.core.{CqlIdentifier, CqlSession}
-//import com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal
-//import com.datastax.oss.driver.api.querybuilder.delete.DeleteSelection
-//import com.datastax.oss.driver.api.querybuilder.insert.InsertInto
-//import com.datastax.oss.driver.api.querybuilder.select.SelectFrom
-//import com.datastax.oss.driver.api.querybuilder.{Literal, QueryBuilder}
-//import zio.flow.cassandra.CassandraKeyValueStore._
-//import zio.flow.internal.KeyValueStore
-//import zio.flow.internal.KeyValueStore.Item
-//import zio.stream.ZStream
-//import zio.{Chunk, IO, Task, URLayer, ZIO, ZLayer}
-//
-//import java.io.IOException
-//import java.nio.ByteBuffer
-//
-//final class CassandraKeyValueStore(session: CqlSession) extends KeyValueStore {
-//
-//  private val keyspace =
-//    session.getKeyspace
-//      .orElse(null: CqlIdentifier) // scalafix:ok DisableSyntax.null
-//
-//  private val cqlSelect: SelectFrom =
-//    QueryBuilder.selectFrom(keyspace, table)
-//
-//  private val cqlInsert: InsertInto =
-//    QueryBuilder.insertInto(keyspace, table)
-//
-//  private val cqlDelete: DeleteSelection =
-//    QueryBuilder.deleteFrom(keyspace, table)
-//
-//  override def put(
-//    namespace: String,
-//    key: Chunk[Byte],
-//    value: Chunk[Byte]
-//  ): IO[IOException, Boolean] = {
-//    val insert = toInsert(Item(namespace, key, value))
-//
-//    executeAsync(insert)
-//      .mapBoth(
-//        refineToIOException(s"Error putting key-value pair for <$namespace> namespace"),
-//        _ => true
-//      )
-//  }
-//
-//  override def get(namespace: String, key: Chunk[Byte]): IO[IOException, Option[Chunk[Byte]]] = {
-//    val query = cqlSelect
-//      .column(valueColumnName)
-//      .whereColumn(namespaceColumnName)
-//      .isEqualTo(
-//        literal(namespace)
-//      )
-//      .whereColumn(keyColumnName)
-//      .isEqualTo(
-//        byteBufferFrom(key)
-//      )
-//      .limit(1)
-//      .build
-//
-//    executeAsync(query).flatMap { result =>
-//      if (result.remaining > 0)
-//        ZIO.attempt {
-//          Option(blobValueOf(valueColumnName, result.one))
-//        }
-//      else
-//        ZIO.none
-//    }.mapError(
-//      refineToIOException(s"Error retrieving or reading value for <$namespace> namespace")
-//    )
-//  }
-//
-//  override def scanAll(namespace: String): ZStream[Any, IOException, (Chunk[Byte], Chunk[Byte])] = {
-//    val query = cqlSelect
-//      .column(keyColumnName)
-//      .column(valueColumnName)
-//      .whereColumn(namespaceColumnName)
-//      .isEqualTo(
-//        literal(namespace)
-//      )
-//      .build
-//
-//    lazy val errorContext =
-//      s"Error scanning all key-value pairs for <$namespace> namespace"
-//
-//    ZStream
-//      .paginateZIO(
-//        executeAsync(query)
-//      )(_.map { result =>
-//        val pairs =
-//          ZStream
-//            .fromJavaIterator(
-//              result.currentPage.iterator
-//            )
-//            .mapZIO { row =>
-//              ZIO.attempt {
-//                blobValueOf(keyColumnName, row) -> blobValueOf(valueColumnName, row)
-//              }
-//            }
-//            .mapError(
-//              refineToIOException(errorContext)
-//            )
-//
-//        val nextPage =
-//          if (result.hasMorePages)
-//            Option(
-//              ZIO.fromCompletionStage(result.fetchNextPage())
-//            )
-//          else
-//            None
-//
-//        (pairs, nextPage)
-//      })
-//      .mapError(
-//        refineToIOException(errorContext)
-//      )
-//      .flatten
-//  }
-//
-//  override def delete(namespace: String, key: Chunk[Byte]): IO[IOException, Unit] = {
-//    val delete = cqlDelete
-//      .column(valueColumnName)
-//      .whereColumn(namespaceColumnName)
-//      .isEqualTo(
-//        literal(namespace)
-//      )
-//      .whereColumn(keyColumnName)
-//      .isEqualTo(
-//        byteBufferFrom(key)
-//      )
-//      .build
-//
-//    executeAsync(delete)
-//      .mapBoth(
-//        refineToIOException(s"Error deleting key-value pair from <$namespace> namespace"),
-//        _ => ()
-//      )
-//  }
-//
-//  override def putAll(items: Chunk[KeyValueStore.Item]): IO[IOException, Unit] = {
-//    val batch = new BatchStatementBuilder(BatchType.LOGGED)
-//    batch.addStatements(
-//      items.map(toInsert): _*
-//    )
-//    executeAsync(batch.build())
-//      .mapBoth(
-//        refineToIOException(s"Error putting multiple key-value pairs"),
-//        _ => ()
-//      )
-//  }
-//
-//  private def toInsert(item: KeyValueStore.Item): SimpleStatement =
-//    cqlInsert
-//      .value(
-//        namespaceColumnName,
-//        literal(item.namespace)
-//      )
-//      .value(
-//        keyColumnName,
-//        byteBufferFrom(item.key)
-//      )
-//      .value(
-//        valueColumnName,
-//        byteBufferFrom(item.value)
-//      )
-//      .build
-//
-//  private def executeAsync(statement: Statement[_]): Task[AsyncResultSet] =
-//    ZIO.fromCompletionStage(
-//      session.executeAsync(statement)
-//    )
-//}
-//
-//object CassandraKeyValueStore {
-//
-//  val layer: URLayer[CqlSession, KeyValueStore] =
-//    ZLayer {
-//      ZIO
-//        .service[CqlSession]
-//        .map(new CassandraKeyValueStore(_))
-//    }
-//
-//  private[cassandra] val tableName: String =
-//    withDoubleQuotes("_zflow_key_value_store")
-//
-//  private[cassandra] val namespaceColumnName: String =
-//    withColumnPrefix("namespace")
-//
-//  private[cassandra] val keyColumnName: String =
-//    withColumnPrefix("key")
-//
-//  private[cassandra] val valueColumnName: String =
-//    withColumnPrefix("value")
-//
-//  private val table: CqlIdentifier =
-//    CqlIdentifier.fromCql(tableName)
-//
-//  private def byteBufferFrom(bytes: Chunk[Byte]): Literal =
-//    literal(
-//      ByteBuffer.wrap(bytes.toArray)
-//    )
-//
-//  private def blobValueOf(columnName: String, row: Row): Chunk[Byte] =
-//    Chunk.fromArray(
-//      row
-//        .getByteBuffer(columnName)
-//        .array
-//    )
-//
-//  private def refineToIOException(errorContext: String): Throwable => IOException = {
-//    case error: IOException =>
-//      error
-//    case error =>
-//      new IOException(s"$errorContext: <${error.getMessage}>.", error)
-//  }
-//
-//  private def withColumnPrefix(s: String) =
-//    withDoubleQuotes("zflow_kv_" + s)
-//
-//  private def withDoubleQuotes(string: String) =
-//    "\"" + string + "\""
-//}
+package zio.flow.cassandra
+
+import com.datastax.oss.driver.api.core.cql._
+import com.datastax.oss.driver.api.core.{CqlIdentifier, CqlSession}
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal
+import com.datastax.oss.driver.api.querybuilder.delete.DeleteSelection
+import com.datastax.oss.driver.api.querybuilder.insert.InsertInto
+import com.datastax.oss.driver.api.querybuilder.select.SelectFrom
+import com.datastax.oss.driver.api.querybuilder.{Literal, QueryBuilder}
+import zio.flow.cassandra.CassandraKeyValueStore._
+import zio.flow.internal.KeyValueStore
+import zio.stream.ZStream
+import zio.{Chunk, IO, Task, URLayer, ZIO, ZLayer}
+
+import java.io.IOException
+import java.nio.ByteBuffer
+import zio.flow.internal.Timestamp
+import com.datastax.oss.driver.api.core.metadata.schema.ClusteringOrder
+import scala.jdk.CollectionConverters._
+
+final class CassandraKeyValueStore(session: CqlSession) extends KeyValueStore {
+
+  private val keyspace =
+    session.getKeyspace
+      .orElse(null: CqlIdentifier) // scalafix:ok DisableSyntax.null
+
+  private val cqlSelect: SelectFrom =
+    QueryBuilder.selectFrom(keyspace, table)
+
+  private val cqlInsert: InsertInto =
+    QueryBuilder.insertInto(keyspace, table)
+
+  private val cqlDelete: DeleteSelection =
+    QueryBuilder.deleteFrom(keyspace, table)
+
+  override def put(
+    namespace: String,
+    key: Chunk[Byte],
+    value: Chunk[Byte],
+    timestamp: Timestamp
+  ): IO[IOException, Boolean] = {
+    val insert = toInsert(namespace, key, timestamp, value)
+
+    executeAsync(insert)
+      .mapBoth(
+        refineToIOException(s"Error putting key-value pair for <$namespace> namespace"),
+        _ => true
+      )
+  }
+
+  private def getLatestImpl(
+    namespace: String,
+    key: Chunk[Byte],
+    before: Option[Timestamp]
+  ): IO[IOException, Option[(Chunk[Byte], Timestamp)]] = {
+    val queryBuilder = cqlSelect
+      .column(valueColumnName)
+      .column(timestampColumnName)
+      .whereColumn(namespaceColumnName)
+      .isEqualTo(
+        literal(namespace)
+      )
+      .whereColumn(keyColumnName)
+      .isEqualTo(
+        byteBufferFrom(key)
+      )
+      .orderBy(
+        Map(
+          keyColumnName       -> ClusteringOrder.DESC,
+          timestampColumnName -> ClusteringOrder.DESC
+        ).asJava
+      )
+
+    val query = before
+      .fold(queryBuilder)(timestamp =>
+        queryBuilder
+          .whereColumn(timestampColumnName)
+          .isLessThanOrEqualTo(
+            literal(timestamp.value)
+          )
+      )
+      .limit(1)
+      .build
+
+    executeAsync(query).flatMap { result =>
+      if (result.remaining > 0) {
+        val element = result.one
+        ZIO.attempt {
+          Option((blobValueOf(valueColumnName, element), Timestamp(element.getLong(timestampColumnName))))
+        }
+      } else {
+        ZIO.none
+      }
+    }.mapError(
+      refineToIOException(s"Error retrieving or reading value for <$namespace> namespace")
+    )
+  }
+
+  override def getLatest(
+    namespace: String,
+    key: Chunk[Byte],
+    before: Option[Timestamp]
+  ): IO[IOException, Option[Chunk[Byte]]] =
+    getLatestImpl(namespace, key, before).map(_.map(_._1))
+
+  override def scanAll(namespace: String): ZStream[Any, IOException, (Chunk[Byte], Chunk[Byte])] = {
+    val query = cqlSelect
+      .column(keyColumnName)
+      .column(valueColumnName)
+      .whereColumn(namespaceColumnName)
+      .isEqualTo(
+        literal(namespace)
+      )
+      .build
+
+    lazy val errorContext =
+      s"Error scanning all key-value pairs for <$namespace> namespace"
+
+    ZStream
+      .paginateZIO(
+        executeAsync(query)
+      )(_.map { result =>
+        val pairs =
+          ZStream
+            .fromJavaIterator(
+              result.currentPage.iterator
+            )
+            .mapZIO { row =>
+              ZIO.attempt {
+                blobValueOf(keyColumnName, row) -> blobValueOf(valueColumnName, row)
+              }
+            }
+            .mapError(
+              refineToIOException(errorContext)
+            )
+
+        val nextPage =
+          if (result.hasMorePages)
+            Option(
+              ZIO.fromCompletionStage(result.fetchNextPage())
+            )
+          else
+            None
+
+        (pairs, nextPage)
+      })
+      .mapError(
+        refineToIOException(errorContext)
+      )
+      .flatten
+  }
+
+  override def getLatestTimestamp(
+    namespace: String,
+    key: zio.Chunk[Byte]
+  ): zio.IO[java.io.IOException, Option[zio.flow.internal.Timestamp]] =
+    getLatestImpl(namespace, key, before = None).map(_.map(_._2))
+
+  private def getAllTimestamps(namespace: String, key: Chunk[Byte]): ZStream[Any, IOException, Timestamp] = {
+    val query = cqlSelect
+      .column(timestampColumnName)
+      .whereColumn(namespaceColumnName)
+      .isEqualTo(
+        literal(namespace)
+      )
+      .whereColumn(keyColumnName)
+      .isEqualTo(
+        byteBufferFrom(key)
+      )
+      .build()
+
+    lazy val errorContext =
+      s"Error scanning all key-value pairs for <$namespace> namespace"
+
+    ZStream
+      .paginateZIO(
+        executeAsync(query)
+      )(_.map { result =>
+        val pairs =
+          ZStream
+            .fromJavaIterator(
+              result.currentPage.iterator
+            )
+            .mapZIO { row =>
+              ZIO.attempt(Timestamp(row.getLong(timestampColumnName)))
+            }
+            .mapError(
+              refineToIOException(errorContext)
+            )
+
+        val nextPage =
+          if (result.hasMorePages)
+            Option(
+              ZIO.fromCompletionStage(result.fetchNextPage())
+            )
+          else
+            None
+
+        (pairs, nextPage)
+      })
+      .mapError(
+        refineToIOException(errorContext)
+      )
+      .flatten
+  }
+
+  override def delete(namespace: String, key: Chunk[Byte]): IO[IOException, Unit] =
+    getAllTimestamps(namespace, key).runCollect.flatMap { timestamps =>
+      val delete = cqlDelete
+        .whereColumn(namespaceColumnName)
+        .isEqualTo(
+          literal(namespace)
+        )
+        .whereColumn(keyColumnName)
+        .isEqualTo(
+          byteBufferFrom(key)
+        )
+        .whereColumn(timestampColumnName)
+        .in(timestamps.map(t => literal(t.value)): _*)
+        .build()
+
+      executeAsync(delete)
+        .mapBoth(
+          refineToIOException(s"Error deleting key-value pair from <$namespace> namespace"),
+          _ => ()
+        )
+    }
+
+  private def toInsert(namespace: String, key: Chunk[Byte], timestamp: Timestamp, value: Chunk[Byte]): SimpleStatement =
+    cqlInsert
+      .value(
+        namespaceColumnName,
+        literal(namespace)
+      )
+      .value(
+        keyColumnName,
+        byteBufferFrom(key)
+      )
+      .value(
+        timestampColumnName,
+        literal(timestamp.value)
+      )
+      .value(
+        valueColumnName,
+        byteBufferFrom(value)
+      )
+      .build
+
+  private def executeAsync(statement: Statement[_]): Task[AsyncResultSet] =
+    ZIO.fromCompletionStage(
+      session.executeAsync(statement)
+    )
+}
+
+object CassandraKeyValueStore {
+
+  val layer: URLayer[CqlSession, KeyValueStore] =
+    ZLayer {
+      ZIO
+        .service[CqlSession]
+        .map(new CassandraKeyValueStore(_))
+    }
+
+  private[cassandra] val tableName: String =
+    withDoubleQuotes("_zflow_key_value_store")
+
+  private[cassandra] val namespaceColumnName: String =
+    withColumnPrefix("namespace")
+
+  private[cassandra] val keyColumnName: String =
+    withColumnPrefix("key")
+
+  private[cassandra] val timestampColumnName: String =
+    withColumnPrefix("timestamp")
+
+  private[cassandra] val valueColumnName: String =
+    withColumnPrefix("value")
+
+  private val table: CqlIdentifier =
+    CqlIdentifier.fromCql(tableName)
+
+  private def byteBufferFrom(bytes: Chunk[Byte]): Literal =
+    literal(
+      ByteBuffer.wrap(bytes.toArray)
+    )
+
+  private def blobValueOf(columnName: String, row: Row): Chunk[Byte] =
+    Chunk.fromArray(
+      row
+        .getByteBuffer(columnName)
+        .array
+    )
+
+  private def refineToIOException(errorContext: String): Throwable => IOException = {
+    case error: IOException =>
+      error
+    case error =>
+      new IOException(s"$errorContext: <${error.getMessage}>.", error)
+  }
+
+  private def withColumnPrefix(s: String) =
+    withDoubleQuotes("zflow_kv_" + s)
+
+  private def withDoubleQuotes(string: String) =
+    "\"" + string + "\""
+}
