26db3c690a2361e51e1cfacdb89be2d73eea9fbc
Albert Peto <petoalbert32@gmail.com>
2022-06-08 16:49:50 +0200

    Add more primitives for query parameters

--- zio-flow/shared/src/main/scala/zio/flow/operation/http/package.scala
+++ zio-flow/shared/src/main/scala/zio/flow/operation/http/package.scala
@@ -7,6 +7,8 @@ import zio.schema.Schema
 
 import java.util.UUID
 import scala.language.implicitConversions
+import java.math.BigDecimal
+import java.math.BigInteger
 
 package object http {
 
@@ -17,9 +19,17 @@ package object http {
   val uuid: Path[UUID]       = Path.Match("uuid", Schema[UUID])
 
   // Query Params
-  def string(name: String): Query[String]   = Query.SingleParam(name, Schema[String])
-  def int(name: String): Query[Int]         = Query.SingleParam(name, Schema[Int])
-  def boolean(name: String): Query[Boolean] = Query.SingleParam(name, Schema[Boolean])
+  def string(name: String): Query[String]         = Query.SingleParam(name, Schema[String])
+  def boolean(name: String): Query[Boolean]       = Query.SingleParam(name, Schema[Boolean])
+  def short(name: String): Query[Short]           = Query.SingleParam(name, Schema[Short])
+  def int(name: String): Query[Int]               = Query.SingleParam(name, Schema[Int])
+  def long(name: String): Query[Long]             = Query.SingleParam(name, Schema[Long])
+  def float(name: String): Query[Float]           = Query.SingleParam(name, Schema[Float])
+  def double(name: String): Query[Double]         = Query.SingleParam(name, Schema[Double])
+  def char(name: String): Query[Char]             = Query.SingleParam(name, Schema[Char])
+  def bigDecimal(name: String): Query[BigDecimal] = Query.SingleParam(name, Schema[BigDecimal])
+  def bigInteger(name: String): Query[BigInteger] = Query.SingleParam(name, Schema[BigInteger])
+  def uuid(name: String): Query[UUID]             = Query.SingleParam(name, Schema[UUID])
 
   implicit def stringToPath(string: String): Path[Unit] = Path.path(string)
 
