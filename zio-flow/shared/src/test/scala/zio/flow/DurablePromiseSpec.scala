5d3fa75bdb5167cdab030bfa18bfdaa770b2fc64
Albert Pető <petoalbert32@gmail.com>
2022-06-16 10:02:39 +0200

    Poll durable promise (#204)
    * Add getAllAvailable method to DurableLog
    
    * Add poll method to DurablePromise

--- zio-flow/shared/src/test/scala/zio/flow/DurablePromiseSpec.scala
+++ zio-flow/shared/src/test/scala/zio/flow/DurablePromiseSpec.scala
@@ -47,6 +47,26 @@ object DurablePromiseSpec extends ZIOSpecDefault {
           _      <- fiber.join
           value  <- result.await
         } yield assertTrue(value == Left("not good"))
+      },
+      test("Can't fail, poll success") {
+        val promise = DurablePromise.make[Nothing, Int]("dp-4")
+        for {
+          log    <- ZIO.service[DurableLog]
+          env     = ZEnvironment(log, execEnv)
+          before <- promise.poll.provideEnvironment(env)
+          _      <- promise.succeed(100).provideEnvironment(env)
+          after  <- promise.poll.provideEnvironment(env)
+        } yield assertTrue(before == None) && assertTrue(after == Some(Right(100)))
+      },
+      test("Can fail, poll failure") {
+        val promise = DurablePromise.make[String, Int]("dp-5")
+        for {
+          log    <- ZIO.service[DurableLog]
+          env     = ZEnvironment(log, execEnv)
+          before <- promise.poll.provideEnvironment(env)
+          _      <- promise.fail("not good").provideEnvironment(env)
+          after  <- promise.poll.provideEnvironment(env)
+        } yield assertTrue(before == None) && assertTrue(after == Some(Left("not good")))
       }
     ).provide(
       DurableLog.live,
