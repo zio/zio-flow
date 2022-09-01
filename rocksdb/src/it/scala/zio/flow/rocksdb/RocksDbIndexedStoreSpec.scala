6bcf4871c42e80dfd91eaad40eba7d23322791be
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-09-01 11:44:00 +0200

    Unified indexed store tests

--- rocksdb/src/it/scala/zio/flow/rocksdb/RocksDbIndexedStoreSpec.scala
+++ rocksdb/src/it/scala/zio/flow/rocksdb/RocksDbIndexedStoreSpec.scala
@@ -20,6 +20,7 @@ import org.{rocksdb => jrocksdb}
 import zio._
 import zio.flow.internal.IndexedStore
 import zio.flow.internal.IndexedStore.Index
+import zio.flow.test.IndexedStoreTests
 import zio.nio.file.{Files, Path}
 import zio.rocksdb.TransactionDB
 import zio.test.Assertion.{containsString, equalTo}
@@ -44,78 +45,30 @@ object RocksDbIndexedStoreSpec extends ZIOSpecDefault {
   private val testIndexedStore: ZLayer[Any, Throwable, IndexedStore] =
     transactionDbPath >>> config >>> RocksDbIndexedStore.layer
 
-  private val suite1 = suite("RocksDbIndexedStore")(
-    test("Test single put") {
-      (for {
-        indexedStore <- ZIO.service[IndexedStore]
-        insertPos    <- indexedStore.put("SomeTopic", Chunk.fromArray("Value1".getBytes()))
-      } yield assertTrue(insertPos == Index(1L))).provide(testIndexedStore)
-    },
-    test("Test sequential put") {
-      (for {
-        indexedStore <- ZIO.service[IndexedStore]
-        posList <- ZIO.foreach((0 until 10).toList)(i =>
-                     indexedStore.put("SomeTopic", Chunk.fromArray(s"Value${i.toString}".getBytes()))
-                   )
-        _ <- ZIO.debug(posList.mkString(","))
-      } yield assertTrue(posList.mkString(",") == "1,2,3,4,5,6,7,8,9,10")).provide(testIndexedStore)
-    },
-    test("Test scan on empty topic") {
-      (for {
-        indexedStore <- ZIO.service[IndexedStore]
-        scannedChunk <- indexedStore.scan("SomeTopic", Index(1L), Index(10L)).runCollect
-        resultChunk  <- ZIO.succeed(scannedChunk.map(bytes => new String(bytes.toArray)))
-      } yield assertTrue(resultChunk.toList.mkString("") == "")).provide(testIndexedStore)
-    },
-    test("Test sequential put and scan") {
-      (for {
-        indexedStore <- ZIO.service[IndexedStore]
-        _ <- ZIO.foreachDiscard((0 until 10).toList) { i =>
-               indexedStore.put("SomeTopic", Chunk.fromArray(s"Value${i.toString}".getBytes()))
-             }
-        scannedChunk <- indexedStore.scan("SomeTopic", Index(1L), Index(10L)).runCollect
-        resultChunk  <- ZIO.succeed(scannedChunk.map(bytes => new String(bytes.toArray)))
-      } yield assertTrue(
-        resultChunk.toList.mkString(",") == "Value0,Value1,Value2,Value3,Value4,Value5,Value6,Value7,Value8,Value9"
-      )).provide(testIndexedStore)
-    },
-    test("Test concurrent put and scan") {
-      val resChunk = (for {
-        indexedStore <- ZIO.service[IndexedStore]
-        _ <- ZIO.foreachParDiscard((0 until 10).toList)(i =>
-               indexedStore.put("SomeTopic", Chunk.fromArray(s"Value${i.toString}".getBytes()))
-             )
-        scannedChunk <- indexedStore.scan("SomeTopic", Index(1L), Index(10L)).runCollect
-        resultChunk  <- ZIO.succeed(scannedChunk.map(bytes => new String(bytes.toArray)))
-      } yield resultChunk).provide(testIndexedStore)
-      assertZIO(resChunk.map(_.size))(equalTo(10)) *>
-        assertZIO(resChunk.map(_.toList.mkString(",")))(containsString("Value9")) *>
-        assertZIO(resChunk.map(_.toList.mkString(",")))(containsString("Value0"))
-    } @@ flaky,
-    test("Get namespaces") {
-      (for {
-        path <- ZIO.service[Path]
-        _    <- ZIO.debug(path.toString())
-        _ <-
-          ZIO
-            .service[IndexedStore]
-            .provide(ZLayer.succeed(path) >>> config >>> RocksDbIndexedStore.withEmptyTopic("someTopic"))
-        ns <-
-          ZIO
-            .serviceWithZIO[TransactionDB](_.initialHandles)
-            .map(_.map(handle => new String(handle.getName, StandardCharsets.UTF_8)))
-            .provide(
-              TransactionDB.liveAllColumnFamilies(
-                new jrocksdb.DBOptions(),
-                new jrocksdb.ColumnFamilyOptions(),
-                new jrocksdb.TransactionDBOptions(),
-                path.toString
-              )
-            ) // Needs to reopen the same DB
-        _ <- ZIO.debug(s"NS: $ns")
-      } yield assertTrue(ns.contains("someTopic"))).provide(transactionDbPath)
-    }
-  )
-
-  override def spec = suite1
+  override def spec: Spec[TestEnvironment, Throwable] =
+    suite("RocksDbIndexedStore")(
+      IndexedStoreTests("Common", initializeDb = ZIO.unit).tests.provideCustomLayer(testIndexedStore),
+      test("Get namespaces") {
+        (for {
+          path <- ZIO.service[Path]
+          _    <- ZIO.debug(path.toString())
+          _ <-
+            ZIO
+              .service[IndexedStore]
+              .provide(ZLayer.succeed(path) >>> config >>> RocksDbIndexedStore.withEmptyTopic("someTopic"))
+          ns <-
+            ZIO
+              .serviceWithZIO[TransactionDB](_.initialHandles)
+              .map(_.map(handle => new String(handle.getName, StandardCharsets.UTF_8)))
+              .provide(
+                TransactionDB.liveAllColumnFamilies(
+                  new jrocksdb.DBOptions(),
+                  new jrocksdb.ColumnFamilyOptions(),
+                  new jrocksdb.TransactionDBOptions(),
+                  path.toString
+                )
+              ) // Needs to reopen the same DB
+        } yield assertTrue(ns.contains("someTopic"))).provide(transactionDbPath)
+      }
+    )
 }
---
+++ test/shared/src/main/scala/zio/flow/test/IndexedStoreTests.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2021-2022 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.flow.test
+
+import zio.{Chunk, Scope, ZIO}
+import zio.flow.internal.IndexedStore
+import zio.flow.internal.IndexedStore.Index
+import zio.test.Assertion.{containsString, equalTo}
+import zio.test.TestAspect.{nondeterministic, sequential}
+import zio.test.{Spec, TestEnvironment, assertTrue, assertZIO, test, suite}
+
+final case class IndexedStoreTests[R](name: String, initializeDb: ZIO[R with Scope, Throwable, Any]) {
+  def tests: Spec[TestEnvironment with R with IndexedStore, Throwable] =
+    suite(name)(
+      test("Test single put") {
+        ZIO.scoped[R with IndexedStore] {
+          for {
+            _            <- initializeDb
+            indexedStore <- ZIO.service[IndexedStore]
+            insertPos    <- indexedStore.put("SomeTopic", Chunk.fromArray("Value1".getBytes()))
+          } yield assertTrue(insertPos == Index(1L))
+        }
+      },
+      test("Test sequential put") {
+        ZIO.scoped[R with IndexedStore] {
+          for {
+            _            <- initializeDb
+            indexedStore <- ZIO.service[IndexedStore]
+            posList <- ZIO.foreach((0 until 10).toList)(i =>
+                         indexedStore.put("SomeTopic", Chunk.fromArray(s"Value${i.toString}".getBytes()))
+                       )
+            _ <- ZIO.debug(posList.mkString(","))
+          } yield assertTrue(posList.mkString(",") == "1,2,3,4,5,6,7,8,9,10")
+        }
+      },
+      test("Test scan on empty topic") {
+        ZIO.scoped[R with IndexedStore] {
+          for {
+            _            <- initializeDb
+            indexedStore <- ZIO.service[IndexedStore]
+            scannedChunk <- indexedStore.scan("SomeTopic", Index(1L), Index(10L)).runCollect
+            resultChunk  <- ZIO.succeed(scannedChunk.map(bytes => new String(bytes.toArray)))
+          } yield assertTrue(resultChunk.toList.mkString("") == "")
+        }
+      },
+      test("Test sequential put and scan") {
+        ZIO.scoped[R with IndexedStore] {
+          for {
+            _            <- initializeDb
+            indexedStore <- ZIO.service[IndexedStore]
+            _ <- ZIO.foreachDiscard((0 until 10).toList) { i =>
+                   indexedStore.put("SomeTopic", Chunk.fromArray(s"Value${i.toString}".getBytes()))
+                 }
+            scannedChunk <- indexedStore.scan("SomeTopic", Index(1L), Index(10L)).runCollect
+            resultChunk  <- ZIO.succeed(scannedChunk.map(bytes => new String(bytes.toArray)))
+          } yield assertTrue(
+            resultChunk.toList.mkString(",") == "Value0,Value1,Value2,Value3,Value4,Value5,Value6,Value7,Value8,Value9"
+          )
+        }
+      },
+      test("Test concurrent put and scan") {
+        val resChunk =
+          ZIO.scoped[R with IndexedStore] {
+            for {
+              _            <- initializeDb
+              indexedStore <- ZIO.service[IndexedStore]
+              _ <- ZIO.foreachParDiscard((0 until 10).toList)(i =>
+                     indexedStore.put("SomeTopic", Chunk.fromArray(s"Value${i.toString}".getBytes()))
+                   )
+              scannedChunk <- indexedStore.scan("SomeTopic", Index(1L), Index(10L)).runCollect
+              resultChunk  <- ZIO.succeed(scannedChunk.map(bytes => new String(bytes.toArray)))
+            } yield resultChunk
+          }
+        assertZIO(resChunk.map(_.size))(equalTo(10)) *>
+          assertZIO(resChunk.map(_.toList.mkString(",")))(containsString("Value9")) *>
+          assertZIO(resChunk.map(_.toList.mkString(",")))(containsString("Value0"))
+      }
+    ) @@ nondeterministic @@ sequential
+}
