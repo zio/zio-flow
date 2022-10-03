3da633bdd68f4aedcd412810f323b7eba16964ec
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-10-03 21:50:27 +0200

    Latest ZIO snapshot

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ object Version {
   val Scala3   = "3.1.0"
   val Silencer = "1.7.8"
 
-  val zio                 = "2.0.2"
+  val zio                 = "2.0.2+78-901cdbd6-SNAPSHOT"
   val zioNio              = "2.0.0"
   val zioAws              = "5.17.224.3"
   val zioRocksDb          = "0.4.1"
