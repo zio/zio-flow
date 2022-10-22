1885fe55283edb3701283c281ea391a4609559e2
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-10-22 12:56:28 +0200

    Http operation metrics

--- zio-flow-runtime/src/main/scala/zio/flow/runtime/internal/HttpRetryLogic.scala
+++ zio-flow-runtime/src/main/scala/zio/flow/runtime/internal/HttpRetryLogic.scala
@@ -1,18 +1,20 @@
 package zio.flow.runtime.internal
 
+import zhttp.http.Status
 import zio.Schedule.forever
 import zio.flow.ActivityError
 import zio.flow.operation.http.{HttpFailure, HttpMethod}
 import zio.flow.runtime.operation.http.{HttpOperationPolicy, HttpRetryCondition, Repetition, RetryLimit}
 import zio._
-import zio.flow.runtime.internal.RetryLogic.{
+import zio.flow.runtime.internal.HttpRetryLogic.{
   conditionToSchedule,
   optionallyJittered,
   repetitionToSchedule,
   retryLimitToSchedule
 }
+import zio.flow.runtime.metrics
 
-class RetryLogic(policy: HttpOperationPolicy, semaphore: Semaphore, circuitBreaker: CircuitBreaker) {
+class HttpRetryLogic(policy: HttpOperationPolicy, semaphore: Semaphore, circuitBreaker: CircuitBreaker) {
   private val schedule =
     policy.retryPolicies.map { retryPolicy =>
       conditionToSchedule(retryPolicy.condition) &&
@@ -31,14 +33,29 @@ class RetryLogic(policy: HttpOperationPolicy, semaphore: Semaphore, circuitBreak
         .whenZIO(circuitBreaker.isOpen)
         .zipRight(operation)
         .zipLeft(circuitBreaker.onSuccess())
-        .retry(schedule)
+        .either
+        .timed
+        .flatMap {
+          case (duration, Left(failure)) => ZIO.fail((duration, failure))
+          case (duration, Right(value))  => ZIO.succeed((duration, value))
+        }
+        .flatMapError { case (duration, failure) =>
+          observeFailure(host, failure, Some(duration), isFinal = false).as(failure)
+        }
+        .flatMap { case (duration, result) =>
+          observeSuccess(host, duration).as(result)
+        }
+        .retry(observeRetries(host, schedule))
+        .tapError(failure => observeFailure(host, failure, None, isFinal = true))
     }.mapError { e =>
       e.failureOption.map(_.toActivityError(method, host)).getOrElse {
         ActivityError(s"$method request to $host died", Some(FiberFailure(e)))
       }
-    }.timeoutFail(ActivityError(s"$method request to $host timed out", None))(
-      policy.timeout
-    )
+    }.timeoutTo(
+      metrics.httpTimedOutRequests(host).increment *>
+        ZIO.fail(ActivityError(s"$method request to $host timed out", None))
+    )(value => ZIO.succeed(value))(policy.timeout)
+      .flatten
 
   private def triggerCircuitBreakerIfNeeded(enabled: Boolean) =
     if (enabled) {
@@ -46,10 +63,44 @@ class RetryLogic(policy: HttpOperationPolicy, semaphore: Semaphore, circuitBreak
     } else {
       Schedule.identity[Cause[HttpFailure]]
     }
+
+  private def observeFailure(
+    host: String,
+    failure: Cause[HttpFailure],
+    duration: Option[Duration],
+    isFinal: Boolean
+  ): ZIO[Any, Nothing, Unit] =
+    failure match {
+      case Cause.Fail(failure @ HttpFailure.Non200Response(status), _) =>
+        metrics.httpResponses(host, status).increment *>
+          ZIO.foreach(duration)(duration => metrics.httpResponseTime(host, status).update(duration)) *>
+          metrics.httpFailedRequests(host, Some(failure), isFinal).increment
+      case Cause.Fail(failure, _) =>
+        metrics.httpFailedRequests(host, Some(failure), isFinal).increment
+      case Cause.Die(_, _) =>
+        metrics.httpFailedRequests(host, None, isFinal).increment
+      case _ =>
+        ZIO.unit
+    }
+
+  private def observeSuccess(host: String, duration: Duration): ZIO[Any, Nothing, Unit] =
+    metrics.httpResponses(host, Status.Ok).increment *>
+      metrics.httpResponseTime(host, Status.Ok).update(duration)
+
+  private def observeRetries(
+    host: String,
+    schedule: Schedule[Any, Cause[HttpFailure], Any]
+  ): Schedule[Any, Cause[HttpFailure], Any] =
+    schedule.onDecision {
+      case (_, _, Schedule.Decision.Continue(_)) =>
+        metrics.httpRetriedRequests(host).increment
+      case _ =>
+        ZIO.unit
+    }
 }
 
-object RetryLogic {
-  def make(policy: HttpOperationPolicy): ZIO[Any, Nothing, RetryLogic] =
+object HttpRetryLogic {
+  def make(policy: HttpOperationPolicy): ZIO[Any, Nothing, HttpRetryLogic] =
     for {
       circuitBreaker <-
         policy.circuitBreakerPolicy match {
@@ -62,10 +113,10 @@ object RetryLogic {
   private[internal] def make(
     policy: HttpOperationPolicy,
     circuitBreaker: CircuitBreaker
-  ): ZIO[Any, Nothing, RetryLogic] =
+  ): ZIO[Any, Nothing, HttpRetryLogic] =
     for {
       semaphore <- Semaphore.make(policy.maxParallelRequestCount.toLong)
-    } yield new RetryLogic(policy, semaphore, circuitBreaker)
+    } yield new HttpRetryLogic(policy, semaphore, circuitBreaker)
 
   private[internal] def conditionToSchedule(condition: HttpRetryCondition): Schedule[Any, Cause[HttpFailure], Any] =
     condition match {
