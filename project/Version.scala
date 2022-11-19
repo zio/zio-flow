b67d10c51c9f75fbffc71e6df3746ecfa6f68b31
Scala Steward <scala_steward@virtuslab.com>
2022-11-19 03:25:30 +0000

    Update zio-schema-optics to 0.3.1

--- project/Version.scala
+++ project/Version.scala
@@ -8,7 +8,7 @@ object Version {
   val zioNio               = "2.0.0"
   val zioAws               = "5.17.224.4"
   val zioRocksDb           = "0.4.2"
-  val zioSchema            = "0.2.1+7-39f57f3e-SNAPSHOT" // TODO: switch back to stable
+  val zioSchema            = "0.3.1" // TODO: switch back to stable
   val awsSdkV1             = "1.12.344"
   val cassandraJavaDriver  = "4.14.1.0"
   val rocksDbJni           = "7.7.3"
