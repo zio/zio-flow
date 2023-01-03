8795a91f6ecb48eba833a7b65e4ee516c88fa105
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-03 10:57:44 +0100

    Update zio-aws-dynamodb, zio-aws-netty to 5.19.7.6 (#378)
    * Update zio-aws-dynamodb, zio-aws-netty to 5.19.7.6
    
    * Explicit zio-prelude version
    
    Co-authored-by: Daniel Vigovszky <daniel.vigovszky@gmail.com>

--- project/Version.scala
+++ project/Version.scala
@@ -6,7 +6,8 @@ object Version {
 
   val zio                  = "2.0.5"
   val zioNio               = "2.0.0"
-  val zioAws               = "5.17.295.22"
+  val zioAws               = "5.19.7.6"
+  val zioPrelude           = "1.0.0-RC16"
   val zioRocksDb           = "0.4.2"
   val zioSchema            = "0.4.1"
   val awsSdkV1             = "1.12.376"
