9067d9079230f5ddce48811d4c247c28c2803e31
Scala Steward <scala_steward@virtuslab.com>
2023-01-03 20:14:03 +0000

    Update zio-aws-dynamodb, zio-aws-netty to 5.19.8.1

--- project/Version.scala
+++ project/Version.scala
@@ -6,7 +6,7 @@ object Version {
 
   val zio                  = "2.0.5"
   val zioNio               = "2.0.0"
-  val zioAws               = "5.19.7.6"
+  val zioAws               = "5.19.8.1"
   val zioPrelude           = "1.0.0-RC16"
   val zioRocksDb           = "0.4.2"
   val zioSchema            = "0.4.1"
