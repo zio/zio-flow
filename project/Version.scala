5bb985aa48486c7b7d7e2688fc8db0b20189fc6f
Scala Steward <scala_steward@virtuslab.com>
2023-01-05 22:47:39 +0000

    Update zio-config, zio-config-typesafe to 3.0.7

--- project/Version.scala
+++ project/Version.scala
@@ -17,6 +17,6 @@ object Version {
   val zioHttp              = "2.0.0-RC11"
   val zioLogging           = "2.1.7"
   val zioMetricsConnectors = "2.0.4"
-  val zioConfig            = "3.0.6"
+  val zioConfig            = "3.0.7"
   val config               = "1.4.2"
 }
