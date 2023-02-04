96c451b966edae54460db96d6f3b86ca8463bcfc
Scala Steward <scala_steward@virtuslab.com>
2023-02-04 21:56:53 +0000

    Update aws-java-sdk-core to 1.12.400

--- project/Version.scala
+++ project/Version.scala
@@ -10,7 +10,7 @@ object Version {
   val zioPrelude           = "1.0.0-RC16"
   val zioRocksDb           = "0.4.2"
   val zioSchema            = "0.4.2"
-  val awsSdkV1             = "1.12.393"
+  val awsSdkV1             = "1.12.400"
   val cassandraJavaDriver  = "4.14.1.0"
   val rocksDbJni           = "7.9.2"
   val testContainers       = "0.40.12"
