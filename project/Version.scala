159227e65f5a5167ef2e6d92a03986044b4067d5
Scala Steward <scala_steward@virtuslab.com>
2023-02-18 21:35:26 +0000

    Update zio-metrics-connectors to 2.0.6

--- project/Version.scala
+++ project/Version.scala
@@ -16,7 +16,7 @@ object Version {
   val testContainers       = "0.40.12"
   val zioHttp              = "2.0.0-RC11"
   val zioLogging           = "2.1.8"
-  val zioMetricsConnectors = "2.0.5"
+  val zioMetricsConnectors = "2.0.6"
   val zioConfig            = "3.0.7"
   val config               = "1.4.2"
 }
