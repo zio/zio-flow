7dcbbaa2df2807adbc621b440999e24639cc754f
Scala Steward <scala_steward@virtuslab.com>
2023-06-20 23:27:35 +0000

    Revert commit(s) 4b8a8a6

--- project/Version.scala
+++ project/Version.scala
@@ -12,7 +12,7 @@ object Version {
   val zioSchema            = "0.4.11"
   val awsSdkV1             = "1.12.464"
   val cassandraJavaDriver  = "4.14.1.0"
-  val rocksDbJni           = "8.1.1.1"
+  val rocksDbJni           = "8.0.0"
   val testContainers       = "0.40.16"
   val zioHttp              = "3.0.0-RC1"
   val zioLogging           = "2.1.11"
