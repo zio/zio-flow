8c2602fbe32207b536857598a7f4a7e7bad3514c
Scala Steward <scala_steward@virtuslab.com>
2024-01-15 06:11:37 +0000

    Update zio, zio-test, zio-test-sbt to 2.0.21

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ object Version {
   val Scala3   = "3.2.1"
   val Silencer = "1.7.12"
 
-  val zio                  = "2.0.15"
+  val zio                  = "2.0.21"
   val zioNio               = "2.0.1"
   val zioAws               = "6.20.42.1"
   val zioPrelude           = "1.0.0-RC19"
