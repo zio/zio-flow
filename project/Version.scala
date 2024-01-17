8d0b71b06e9b806df6fc2f6e4fc347d356d17dcb
Scala Steward <scala_steward@virtuslab.com>
2024-01-17 05:07:57 +0000

    Update zio-prelude to 1.0.0-RC22

--- project/Version.scala
+++ project/Version.scala
@@ -7,7 +7,7 @@ object Version {
   val zio                  = "2.0.15"
   val zioNio               = "2.0.1"
   val zioAws               = "6.20.42.1"
-  val zioPrelude           = "1.0.0-RC19"
+  val zioPrelude           = "1.0.0-RC22"
   val zioRocksDb           = "0.4.3"
   val zioSchema            = "0.4.12"
   val awsSdkV1             = "1.12.459"
